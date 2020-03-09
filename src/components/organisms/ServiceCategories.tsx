245581ca9356108dcd0865f336620422d081420c
Juraj Piar <juraj.piar@hotmail.com>
2020-03-09 12:50:49 +0100

    feat: adds buttons for menu [skip ci]

--- src/components/organisms/ServiceCategories.tsx
+++ src/components/organisms/ServiceCategories.tsx
@@ -2,31 +2,37 @@ import React from 'react';
 import IconedItem, { IconedItemProps } from 'components/molecules/IconedItem';
 import { IconsEnum } from 'components/atoms/Icon';
 import Heading from 'components/atoms/Heading';
+import { ROUTES } from 'routes';
 
 function ServiceCategories() {
   const availableServices: IconedItemProps[] = [
     {
       className: 'nameService',
+      to: ROUTES.DOMAINS,
       text: 'Name Service',
       iconProps: { name: IconsEnum.BLANK },
     },
     {
       className: 'storage',
+      to: ROUTES.STORAGE,
       text: 'Storage',
       iconProps: { name: IconsEnum.BLANK },
     },
     {
       className: 'payments',
+      to: ROUTES.LANDING,
       text: 'Payments',
       iconProps: { name: IconsEnum.BLANK },
     },
     {
       className: 'dataService',
+      to: ROUTES.LANDING,
       text: 'Data Services',
       iconProps: { name: IconsEnum.BLANK },
     },
     {
       className: 'communications',
+      to: ROUTES.LANDING,
       text: 'Communications',
       iconProps: { name: IconsEnum.BLANK },
     },
