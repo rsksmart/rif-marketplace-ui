49a39637ca01f7f923b71e9e6d5a63eb3ad9b493
Alvaro Fariña <farinalvaro@gmail.com>
2020-03-23 17:36:31 -0300

    feat: Installed @material-ui/icons and created Accordion component

--- src/rifui/index.ts
+++ src/rifui/index.ts
@@ -1,12 +1,14 @@
 // CSS files
 import './css/style.css';
 
+import Accordion from './components/Accordion';
 import Logo from './Logo';
 import LogoFooter from './LogoFooter';
 import LogoNavbar from './LogoNavbar';
 import RangeSlider from './components/RangeSlider';
 
 export {
+    Accordion,
     Logo,
     LogoFooter,
     LogoNavbar,
