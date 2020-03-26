39c84795e4bf60b0e68b1e291c44083d175fa3d8
Alvaro Fariña <farinalvaro@gmail.com>
2020-03-26 16:10:44 -0300

    refactor: deleted un-used vars and references

--- src/store/User/userActions.ts
+++ src/store/User/userActions.ts
@@ -1,16 +1,10 @@
-// import { Contact, User } from 'models'
 import { IAction } from 'store/storeUtils/interfaces'
-import LocalStorage from 'utils/LocalStorage'
-import { IUserState } from './UserStore'
 import User from 'models/User'
 
 export enum USER_ACTIONS {
   LOGIN = 'LOGIN',
   LOGOUT = 'LOGOUT',
 }
-
-const persistence = LocalStorage.getInstance()
-
 
 export type UserPayload = INodePayload
 
