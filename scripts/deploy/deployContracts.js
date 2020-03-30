4051b850ea0d80e92e41c45731f369e20a2ab43a
Vojtech Simetka <vojtech@simetka.cz>
2020-03-30 23:43:41 +0200

    feat: the process is now more interactive and can write `.env` file

--- scripts/deploy/deployContracts.js
+++ scripts/deploy/deployContracts.js
@@ -15,7 +15,7 @@ const printBalances = async (web3, accounts) =>
   );
 
 /**
- * Deploy a smart contract to the blockchain
+ * Helper function to deploy a smart contract to the blockchain
  *
  * @param web3      Instance of Web3
  * @param account   Account from which the contract should be deployed
@@ -39,48 +39,60 @@ const deploy = async (web3, account, contract, arguments = []) => {
     });
 };
 
+/**
+ * Deploys all the smart contracts needed
+ *
+ * @return Returns object with deployed contracts
+ */
 module.exports = async web3 => {
   const accounts = await web3.eth.getAccounts();
   const defaultToken = web3.utils.sha3('DEFAULT_TOKEN');
 
-  // const erc20 = await deploy(web3, accounts[0], ERC20Mintable);
-  // console.log(`ERC20 token deployed to ${erc20.options.address}`);
-  // await erc20.methods
-  //   .mint(accounts[1], web3.utils.toBN('1000000000000000000000'))
-  //   .send({
-  //     from: accounts[0],
-  //     gasPrice: web3.utils.toWei('1', 'gwei'),
-  //   });
+  // Create and mint ERC20 token
+  const erc20 = await deploy(web3, accounts[0], ERC20Mintable);
+  console.log(`ERC20 token (used as DOC) deployed to ${erc20.options.address}`);
+  await erc20.methods
+    .mint(accounts[0], web3.utils.toBN('1000000000000000000000'))
+    .send({
+      from: accounts[0],
+      gasPrice: web3.utils.toWei('1', 'gwei'),
+    });
+  console.log(`10^21 ERC20 token (used as DOC) minted`);
 
+  // Create and mint ERC677 token
   const erc677 = await deploy(web3, accounts[0], ERC677, [
-    accounts[1],
+    accounts[0],
     web3.utils.toBN('1000000000000000000000'),
     'RIF',
     'RIF',
     web3.utils.toBN('18'),
   ]);
-  console.log(`ERC677 token deployed to ${erc677.options.address}`);
+  console.log(`ERC677 token (used as RIF) deployed to ${erc677.options.address}`);
 
-  // const erc777 = await deploy(web3, accounts[0], ERC777, [
-  //   'ERC 721',
-  //   '721',
-  //   [],
-  // ]);
-  // console.log(`ERC777 token deployed to ${erc777.options.address}`);
-  // await erc777.methods
-  //   .mint(accounts[1], web3.utils.toBN('1000000000000000000000'))
-  //   .send({
-  //     from: accounts[0],
-  //     gasPrice: web3.utils.toWei('1', 'gwei'),
-  //   });
+  // Create and mint ERC777 token
+  const erc777 = await deploy(web3, accounts[0], ERC777, [
+    'ERC 777',
+    '777',
+    [],
+  ]);
+  console.log(`ERC777 token (used as DAI) deployed to ${erc777.options.address}`);
+  await erc777.methods
+    .mint(accounts[0], web3.utils.toBN('1000000000000000000000'))
+    .send({
+      from: accounts[0],
+      gasPrice: web3.utils.toWei('1', 'gwei'),
+    });
+  console.log(`10^21 ERC777 tokens (used as DAI) minted`);
 
+  // Create RNS token contract
   const erc721 = await deploy(web3, accounts[0], ERC721Mintable);
-  console.log(`ERC721 token deployed to ${erc721.options.address}`);
+  console.log(`ERC721 token (NFT we use for RNS) deployed to ${erc721.options.address}`);
   await erc721.methods.mint(accounts[0], defaultToken).send({
     from: accounts[0],
     gasPrice: web3.utils.toWei('1', 'gwei'),
   });
 
+  // Marketplace registry contract
   const erc721SimplePlacements = await deploy(
     web3,
     accounts[0],
@@ -92,9 +104,17 @@ module.exports = async web3 => {
   );
 
   await erc721SimplePlacements.methods
-    .setWhitelistedPaymentToken(erc677.options.address, true, false, false)
+    .setWhitelistedPaymentToken(erc677.options.address, false, true, false)
     .send({
       from: accounts[0],
     });
-  console.log(`ERC677 token whitelisted for payments`);
+  console.log(`ERC677 token (used as RIF) whitelisted for payments`);
+
+  return {
+    erc20: erc20.options.address,
+    erc677: erc677.options.address,
+    erc721: erc721.options.address,
+    erc777: erc777.options.address,
+    erc721SimplePlacements: erc721SimplePlacements.options.address
+  }
 };
--- scripts/deployProcess.js
+++ scripts/deployProcess.js
@@ -17,7 +17,34 @@ async function deployProcess() {
   console.log('Using accounts:\n', accounts, '\n');
 
   // Deploy all the contracts
-  await deployContracts(web3);
+  const {
+    erc20,
+    erc677,
+    erc721,
+    erc777,
+    erc721SimplePlacements,
+  } = await deployContracts(web3);
+
+  if (child) {
+    console.log(`Stopping the child node process`);
+    child.kill('SIGINT');
+  }
+
+  const configuration = {
+    REACT_APP_DOC: erc20,
+    REACT_APP_RIF: erc677,
+    REACT_APP_DAI: erc777,
+    REACT_APP_RNS: erc721,
+    REACT_APP_MARKETPLACE_TCR: erc721SimplePlacements,
+  };
+
+  const queryWriteConfiguration = await new Confirm(
+    'Write the configuration files?',
+  ).run();
+  if (queryWriteConfiguration) {
+    const out = Object.entries(configuration).map(([k, e]) => `${k}=${e}`);
+    await fs.writeFileSync('.env', out.join('\n'));
+  }
 
   process.exit();
 }
