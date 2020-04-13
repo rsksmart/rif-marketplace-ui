ce68f792a58ccbc12fa61aa277d52ccc95884411
Juraj Piar <juraj@iovlabs.org>
2020-04-13 08:54:40 +0100

    refactor: removes duplicated component

--- src/rifui/components/atoms/forms/index.ts
+++ src/rifui/components/atoms/forms/index.ts
@@ -1,7 +1,6 @@
 import Checkbox from './Checkbox';
 import FormControl from './FormControl';
 import FormGroup from './FormGroup';
-import Grid from './Grid';
 import Input from './Input';
 import InputAdornment from './InputAdornment';
 import InputLabel from './InputLabel';
@@ -13,7 +12,6 @@ export {
 	Checkbox,
 	FormControl,
 	FormGroup,
-	Grid,
 	Input,
 	InputAdornment,
 	InputLabel,
