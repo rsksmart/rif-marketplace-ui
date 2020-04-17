1105e1c911240da8d95dd9aaf8cf825bcdc1a5a7
Vojtech Simetka <vojtech@simetka.cz>
2020-04-17 16:22:09 +0200

    ci: added linter

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import React from 'react';
-import App from './App';
-
-test('renders wrapper', () => {
-  const { getByTestId } = render(<App />);
-  const wrapperElement = getByTestId('wrapper');
-  expect(wrapperElement).toBeInTheDocument();
-});
