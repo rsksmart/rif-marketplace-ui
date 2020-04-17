0fa751e69ab709fb75a53ab9fb6295586953995b
Juraj Piar <juraj@iovlabs.org>
2020-04-17 18:36:05 +0100

    feat: makes user to sign to wallet

--- src/components/pages/rns/DomainsBuyPage.tsx
+++ src/components/pages/rns/DomainsBuyPage.tsx
@@ -9,6 +9,7 @@ import { useHistory } from 'react-router';
 import { ROUTES } from 'routes';
 import { MARKET_ACTIONS } from 'store/Market/marketActions';
 import MarketStore, { TxType } from 'store/Market/MarketStore';
+import { Web3Store } from 'rifui/providers/Web3Provider';
 
 const LISTING_TYPE = MarketListingTypes.DOMAIN_OFFERS;
 const TX_TYPE = TxType.BUY;
@@ -38,6 +39,7 @@ const DomainsBuyPage = () => {
       })
     }
   })
+
   useEffect(() => {
     if (!servicePath) {
       const serviceAddr = createOffersService();
--- src/components/templates/MarketPageTemplate.tsx
+++ src/components/templates/MarketPageTemplate.tsx
@@ -1,8 +1,13 @@
-import React, { FC } from 'react';
+import React, { FC, useContext } from 'react';
 import MarketFilter from 'components/templates/marketplace/MarketFilter';
 import Marketplace, { TableHeaders } from 'components/templates/marketplace/Marketplace';
-import { MarketItemType, MarketListingTypes } from 'models/Market';
+import { MarketItemType } from 'models/Market';
 import { makeStyles, Theme } from '@material-ui/core/styles';
+
+// TODO: use MaterialUI classes
+import './MarketPageTemplate.css';
+import { Web3Store } from 'rifui/providers/Web3Provider';
+
 
 export interface MarketPageTemplateProps {
   className: string;
@@ -25,11 +30,20 @@ const MarketPageTemplate: FC<MarketPageTemplateProps> = ({
   itemCollection,
   headers,
 }) => {
+  const {
+    state: { account },
+  } = useContext(Web3Store);
   const classes = useStyles();
+
   return (
     <div className={`${classes.root} ${className}`}>
-      <MarketFilter>{filterItems}</MarketFilter>
-      <Marketplace items={itemCollection} headers={headers} />
+      {!account && <p>Please sign in to your wallet</p>}
+      {account &&
+        <>
+          <MarketFilter>{filterItems}</MarketFilter>
+          <Marketplace items={itemCollection} headers={headers} />
+        </>
+      }
     </div>
   );
 };
