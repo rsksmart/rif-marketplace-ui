f66bc17a160dfcb4e43f5b463c74f37faf1657aa
Alvaro Fariña <farinalvaro@gmail.com>
2020-04-20 20:32:49 -0300

    feat/refactor: added styles to the modal content

--- src/rifui/components/atoms/modal/ModalHeader.tsx
+++ src/rifui/components/atoms/modal/ModalHeader.tsx
@@ -9,6 +9,9 @@ export interface ModalHeaderProps {
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
     borderBottom: `1px solid ${colors.gray2}`,
+    color: colors.primary,
+    display: 'flex',
+    justifyContent: 'center',
     width: '100%'
   }
 }));
