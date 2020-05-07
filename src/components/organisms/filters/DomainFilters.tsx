057262e29dd7c4d768268be654068b89f1bbf5d2
Juraj Piar <juraj@iovlabs.org>
2020-05-07 12:39:23 +0100

    feat: adds filter radio buttons

--- src/components/organisms/filters/DomainFilters.tsx
+++ src/components/organisms/filters/DomainFilters.tsx
@@ -1,7 +1,8 @@
-import React, { useContext } from "react";
-import MarketStore from "store/Market/MarketStore";
-import SearchFilter from "./SearchFilter";
+import React, { useContext, useState } from "react";
 import { MARKET_ACTIONS } from "store/Market/marketActions";
+import MarketStore from "store/Market/MarketStore";
+import RadioFilter from "./RadioFilter";
+import SearchFilter from "./SearchFilter";
 
 const DomainFilters = () => {
     const {
@@ -9,14 +10,29 @@ const DomainFilters = () => {
             filters: {
                 domains: {
                     name: nameFilter,
-                },
+                    status: statusFilter,
+                }
             },
         },
         dispatch,
     } = useContext(MarketStore);
 
+    const [status, setStatus] = useState(statusFilter || 'owned')
     const searchValue = (nameFilter && nameFilter.$like) || '';
 
+
+    const domainStatusFilters = [{
+        value: 'owned',
+        label: 'Your domains',
+    },
+    {
+        value: 'onsale',
+        label: 'Your offers',
+    },
+    {
+        value: 'sold',
+        label: 'Sold domains',
+    }];
     return (<>
         <SearchFilter
             value={searchValue}
@@ -29,6 +45,22 @@ const DomainFilters = () => {
                     payload: {
                         filterItems: {
                             name,
+                        }
+                    }
+                })
+            }}
+        />
+        <RadioFilter
+            title='Domain Status'
+            items={domainStatusFilters}
+            value={status}
+            onChange={({ target: { value } }) => {
+                setStatus(value);
+                dispatch({
+                    type: MARKET_ACTIONS.SET_FILTER,
+                    payload: {
+                        filterItems: {
+                            status: value
                         }
                     }
                 })
