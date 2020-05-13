dde4d09b2da8436dfe70e337701e911781baca3d
Alvaro Fariña <farinalvaro@gmail.com>
2020-05-13 15:53:58 -0300

    feat: added Storage content and ImageSide Template

---
+++ src/components/templates/SideImageTemplate.tsx
@@ -0,0 +1,70 @@
+import React, { FC } from 'react';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import { Typography, Grid } from '@rsksmart/rif-ui';
+
+export interface SideImageTemplateProps {
+  mainTitle: string;
+  sideIcon: React.ReactElement;
+  sideText: React.ReactElement;
+};
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop: theme.spacing(4)
+  },
+  container: {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    maxWidth: '90%',
+    [theme.breakpoints.up('md')]: {
+      maxWidth: '80%',
+    },
+  },
+  gridContainer: {
+    justifyContent: 'center',
+    display: 'flex',
+  },
+  mainTitle: {
+    fontSize: theme.typography.pxToRem(50),
+    marginBottom: theme.spacing(4)
+  },
+  content: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    margin: theme.spacing(2, 0)
+  },
+  textContent: {
+    maxWidth: '90%',
+    [theme.breakpoints.up('md')]: {
+      maxWidth: '80%',
+    },
+  }
+}));
+
+const SideImageTemplate: FC<SideImageTemplateProps> = ({ mainTitle, sideIcon, sideText }) => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <Typography className={classes.mainTitle} color='primary' variant='h1'>{mainTitle}</Typography>
+        <Grid container className={classes.gridContainer}>
+          <Grid item className={classes.content} sm={12} md={6} lg={4}>
+            {sideIcon}
+          </Grid>
+          <Grid item className={classes.content} sm={12} md={6} lg={8}>
+            <div className={classes.textContent}>
+              {sideText}
+            </div>
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
+};
+
+export default SideImageTemplate;
