a523641af0a3285c087cf129e444c7fe15a27b3a
Alvaro Fariña <farinalvaro@gmail.com>
2020-05-14 09:51:12 -0300

    fix: updated content on FAQ and footer

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -21,17 +21,18 @@ const Footer = () => {
         links: [{
           title: 'Services',
           to: 'https://developers.rsk.co/rif/',
+          target: '_blank',
           isExternal: true
         },
         {
           title: 'Whitepaper',
-          to: 'https://developers.rsk.co/rif/',
+          to: 'https://www.rifos.org/assets/whitepapers/rif-whitepaper-en.pdf',
           target: '_blank',
           isExternal: true
         },
         {
           title: 'RIF token',
-          to: 'https://developers.rsk.co/rif/',
+          to: 'https://developers.rsk.co/rif/token/',
           target: '_blank',
           isExternal: true
         }]
@@ -39,20 +40,20 @@ const Footer = () => {
       {
         title: 'RIF Marketplace',
         links: [{
-          title: 'Services',
-          to: 'https://developers.rsk.co/rif/',
+          title: 'Roadmap',
+          to: 'https://www.rifos.org/marketplace#roadmap',
           target: '_blank',
           isExternal: true
         },
         {
-          title: 'Whitepaper',
-          to: 'https://developers.rsk.co/rif/',
+          title: 'Articles',
+          to: 'https://www.rifos.org/blog',
           target: '_blank',
           isExternal: true
         },
         {
-          title: 'RIF token',
-          to: 'https://developers.rsk.co/rif/',
+          title: 'Follow us',
+          to: 'https://twitter.com/rif_os',
           target: '_blank',
           isExternal: true
         }]
@@ -60,20 +61,20 @@ const Footer = () => {
       {
         title: 'Developers',
         links: [{
-          title: 'Services',
+          title: 'Setup Guide',
+          to: 'https://github.com/rsksmart/rif-marketplace-dev/blob/master/README.md',
+          target: '_blank',
+          isExternal: true
+        },
+        {
+          title: 'Docs',
           to: 'https://developers.rsk.co/rif/',
           target: '_blank',
           isExternal: true
         },
         {
-          title: 'Whitepaper',
-          to: 'https://developers.rsk.co/rif/',
-          target: '_blank',
-          isExternal: true
-        },
-        {
-          title: 'RIF token',
-          to: 'https://developers.rsk.co/rif/',
+          title: 'Github',
+          to: 'https://github.com/rsksmart?q=rif-marketplace',
           target: '_blank',
           isExternal: true
         }]
@@ -81,20 +82,14 @@ const Footer = () => {
       {
         title: 'Privacy',
         links: [{
-          title: 'Services',
-          to: 'https://developers.rsk.co/rif/',
+          title: 'Privacy Policy',
+          to: 'https://www.rifos.org/privacy-policy',
           target: '_blank',
           isExternal: true
         },
         {
-          title: 'Whitepaper',
-          to: 'https://developers.rsk.co/rif/',
-          target: '_blank',
-          isExternal: true
-        },
-        {
-          title: 'RIF token',
-          to: 'https://developers.rsk.co/rif/',
+          title: 'Terms & Conditions',
+          to: 'https://www.rifos.org/terms-conditions',
           target: '_blank',
           isExternal: true
         }]
