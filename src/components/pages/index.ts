a4ecb285f7ee9237c59c287fe13da487d6f120da
Alvaro Fariña <farinalvaro@gmail.com>
2020-05-20 17:45:16 -0300

    refactor: moved FAQ stuff to rif-ui and re-organized imports

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -1,1 +1,12 @@
+import AboutPage from './AboutPage';
+import FAQPage from './FAQPage';
+import LandingPage from './LandingPage';
+import NotFound from './NotFound';
+
+export {
+  AboutPage,
+  FAQPage,
+  LandingPage,
+  NotFound,
+};
 export * from './rns'
