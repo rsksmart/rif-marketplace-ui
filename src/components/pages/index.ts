05594261e831492de7cb40a9030c8d9403272f09
Alvaro Fariña <farinalvaro@gmail.com>
2020-05-21 16:38:07 -0300

    fix: fixed lint issues

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -1,12 +1,12 @@
-import AboutPage from './AboutPage';
-import FAQPage from './FAQPage';
-import LandingPage from './LandingPage';
-import NotFound from './NotFound';
+import AboutPage from './AboutPage'
+import FAQPage from './FAQPage'
+import LandingPage from './LandingPage'
+import NotFound from './NotFound'
 
 export {
   AboutPage,
   FAQPage,
   LandingPage,
   NotFound,
-};
+}
 export * from './rns'
