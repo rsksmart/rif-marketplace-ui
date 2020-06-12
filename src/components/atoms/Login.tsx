8135d002b95926510767e8c3ca49253ef539966b
Alvaro Fariña <farinalvaro@gmail.com>
2020-06-12 15:12:27 -0300

    feat: navigate to landing on account changes

--- src/components/atoms/Login.tsx
+++ src/components/atoms/Login.tsx
@@ -1,20 +1,30 @@
 import React from 'react'
 import { Account, Web3Provider } from '@rsksmart/rif-ui'
+import { useHistory } from 'react-router-dom'
+import ROUTES from 'routes'
 
 const requiredNetworkId = process.env.REQUIRED_NETWORK || 31
 
 const Login = () => {
 
+  const history = useHistory()
   return (
     <Web3Provider.Consumer>
-      {({ state: { web3, account, networkInfo }, actions: { setProvider } }) => (
-        <Account
-          web3={web3}
-          account={account}
-          setProvider={setProvider}
-          requiredNetwork={requiredNetworkId}
-          currentNetwork={networkInfo?.networkId}
-        />
+      {({ state: { web3, account, networkInfo }, actions: { setProvider, registerOnAccountsChange } }) => (
+        <>
+          {
+            registerOnAccountsChange(() => {
+              history.push(ROUTES.LANDING)
+            })
+          }
+          <Account
+            web3={web3}
+            account={account}
+            setProvider={setProvider}
+            requiredNetwork={requiredNetworkId}
+            currentNetwork={networkInfo?.networkId}
+          />
+        </>
       )}
     </Web3Provider.Consumer>
   )
