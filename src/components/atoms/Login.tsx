f675f2c252558ce5e7353db33d723b6632368c7d
Alvaro Fariña <farinalvaro@gmail.com>
2020-06-16 10:38:54 -0300

    fix: fixed lint issues

--- src/components/atoms/Login.tsx
+++ src/components/atoms/Login.tsx
@@ -3,21 +3,18 @@ import { Account, Web3Provider } from '@rsksmart/rif-ui'
 
 const requiredNetworkId = process.env.REQUIRED_NETWORK || 31
 
-const Login = () => {
-
-  return (
-    <Web3Provider.Consumer>
-      {({ state: { web3, account, networkInfo }, actions: { setProvider } }) => (
-        <Account
-          web3={web3}
-          account={account}
-          setProvider={setProvider}
-          requiredNetworkId={requiredNetworkId}
-          currentNetworkId={networkInfo?.networkId}
-        />
-      )}
-    </Web3Provider.Consumer>
-  )
-}
+const Login = () => (
+  <Web3Provider.Consumer>
+    {({ state: { web3, account, networkInfo }, actions: { setProvider } }) => (
+      <Account
+        web3={web3}
+        account={account}
+        setProvider={setProvider}
+        requiredNetworkId={requiredNetworkId}
+        currentNetworkId={networkInfo?.networkId}
+      />
+    )}
+  </Web3Provider.Consumer>
+)
 
 export default Login
