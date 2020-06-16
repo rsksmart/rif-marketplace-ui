d5ff74d21d6a78f61ab9bab6db0b8029719c2448
Alvaro Fariña <farinalvaro@gmail.com>
2020-06-15 23:06:24 -0300

    feat: adapted to new version of rif-ui - handled account and network changes

--- src/components/atoms/Login.tsx
+++ src/components/atoms/Login.tsx
@@ -1,39 +1,22 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Account, Web3Provider } from '@rsksmart/rif-ui'
-import { useHistory } from 'react-router-dom'
-import ROUTES from 'routes'
-import { Alert } from '@material-ui/lab'
 
 const requiredNetworkId = process.env.REQUIRED_NETWORK || 31
 
 const Login = () => {
 
-  const history = useHistory()
-  const [displayAlert, setDisplayAlert] = useState(false)
-
   return (
-    <>
-      {displayAlert && <Alert severity='warning' onClose={() => setDisplayAlert(false)}>Your account has changed</Alert>}
-      <Web3Provider.Consumer>
-        {({ state: { web3, account, networkInfo }, actions: { setProvider, registerOnAccountsChange } }) => (
-          <>
-            {
-              registerOnAccountsChange(() => {
-                setDisplayAlert(true)
-                history.push(ROUTES.LANDING)
-              })
-            }
-            <Account
-              web3={web3}
-              account={account}
-              setProvider={setProvider}
-              requiredNetwork={requiredNetworkId}
-              currentNetwork={networkInfo?.networkId}
-            />
-          </>
-        )}
-      </Web3Provider.Consumer>
-    </>
+    <Web3Provider.Consumer>
+      {({ state: { web3, account, networkInfo }, actions: { setProvider } }) => (
+        <Account
+          web3={web3}
+          account={account}
+          setProvider={setProvider}
+          requiredNetworkId={requiredNetworkId}
+          currentNetworkId={networkInfo?.networkId}
+        />
+      )}
+    </Web3Provider.Consumer>
   )
 }
 
