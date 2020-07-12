0735d669e9179c8af0c0818d826e6b403a3ddfb1
Juraj Piar <juraj@iovlabs.org>
2020-07-12 22:13:22 +0100

    feat: changes type of message to obj with type, text and custom action removes the necessity to connect wallet to sell instead it shows alert adds ErrorPanel component

--- src/components/organisms/rns/sell/MyDomains.tsx
+++ src/components/organisms/rns/sell/MyDomains.tsx
@@ -83,7 +83,7 @@ const MyDomains: FC<{}> = () => {
       filterItems={<DomainFilters />}
       itemCollection={collection}
       headers={headers}
-      accountRequired
+      requiresAccount
       dispatch={dispatch}
       outdatedCt={listing.outdatedTokens.length}
     />
--- src/components/templates/MarketPageTemplate.tsx
+++ src/components/templates/MarketPageTemplate.tsx
@@ -4,8 +4,11 @@ import { Web3Store } from '@rsksmart/rif-ui'
 import InfoBar from 'components/molecules/InfoBar'
 import MarketFilter from 'components/templates/marketplace/MarketFilter'
 import { MarketItem } from 'models/Market'
-import React, { Dispatch, FC, useContext } from 'react'
+import React, {
+  Dispatch, FC, useContext, useEffect,
+} from 'react'
 import { RnsAction } from 'store/Market/rns/rnsActions'
+import AppStore from 'store/App/AppStore'
 import Marketplace, { TableHeaders } from './marketplace/Marketplace'
 
 export interface MarketPageTemplateProps {
@@ -13,7 +16,7 @@ export interface MarketPageTemplateProps {
   filterItems: React.ReactNode
   headers: TableHeaders
   itemCollection: MarketItem[]
-  accountRequired?: boolean
+  requiresAccount?: boolean
   dispatch: Dispatch<RnsAction>
   outdatedCt: number
 }
@@ -40,7 +43,7 @@ const MarketPageTemplate: FC<MarketPageTemplateProps> = ({
   filterItems,
   itemCollection,
   headers,
-  accountRequired,
+  requiresAccount,
   dispatch,
   outdatedCt,
 }) => {
@@ -48,35 +51,48 @@ const MarketPageTemplate: FC<MarketPageTemplateProps> = ({
   const {
     state: { account },
   } = useContext(Web3Store)
+  const { dispatch: appDispatch } = useContext(AppStore)
+
+  useEffect(() => {
+    if (requiresAccount && !account) {
+      appDispatch({
+        type: 'SET_MESSAGE',
+        payload: {
+          type: 'warning',
+          text: 'Please, connect your wallet.',
+          // customAction: {
+          //   name: 'Connect',
+          //   action: () => {
+          //     return <Login /> //TODO: dispatch web3dispatch action 'CONNECT' that would trigger the wallet connection modal to open
+          //   }
+          // }
+        },
+      })
+    }
+  }, [appDispatch, requiresAccount, account])
 
   return (
     <Grid container direction="row" className={`${classes.root} ${className}`}>
-      {accountRequired && !account && <p>Please sign in to your wallet</p>}
-      {(!accountRequired || account)
-        && (
-          <>
-            <Grid className={classes.filtersContainer} item sm={12} md={3}>
-              <MarketFilter>{filterItems}</MarketFilter>
-            </Grid>
-            <Grid className={classes.resultsContainer} item sm={12} md={9}>
-              <InfoBar
-                isVisible={!!outdatedCt}
-                text={`${outdatedCt} item(s) in this listing had been updated. Please,`}
-                buttonText="refresh"
-                type="info"
-                button={{
-                  onClick: () => {
-                    dispatch({
-                      type: 'REFRESH',
-                      payload: { refresh: true },
-                    } as any)
-                  },
-                }}
-              />
-              <Marketplace items={itemCollection} headers={headers} />
-            </Grid>
-          </>
-        )}
+      <Grid className={classes.filtersContainer} item sm={12} md={3}>
+        <MarketFilter>{filterItems}</MarketFilter>
+      </Grid>
+      <Grid className={classes.resultsContainer} item sm={12} md={9}>
+        <InfoBar
+          isVisible={!!outdatedCt}
+          text={`${outdatedCt} item(s) in this listing had been updated. Please,`}
+          buttonText="refresh"
+          type="info"
+          button={{
+            onClick: () => {
+              dispatch({
+                type: 'REFRESH',
+                payload: { refresh: true },
+              } as any)
+            },
+          }}
+        />
+        <Marketplace items={itemCollection} headers={headers} />
+      </Grid>
     </Grid>
   )
 }
