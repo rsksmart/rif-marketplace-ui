4811cddf8efa6dc4a502a97b3ce6c61e134cfd45
Alvaro Fariña <farinalvaro@gmail.com>
2020-07-15 15:58:52 -0300

    feat: updated inputs to number type and set suggested month

--- src/components/organisms/storage/listing/BaseSettings.tsx
+++ src/components/organisms/storage/listing/BaseSettings.tsx
@@ -1,22 +1,37 @@
-import React, { FC } from 'react'
-import { Grid, MenuItem, TextField, InputAdornment, Typography, Tooltip } from '@material-ui/core'
+import React, { FC, useState } from 'react'
+
 import InfoIcon from '@material-ui/icons/Info';
-import { colors } from '@rsksmart/rif-ui'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Tooltip from '@material-ui/core/Tooltip';
+
+import { colors, validatedNumber } from '@rsksmart/rif-ui'
+import CountrySelect from 'components/molecules/storage/CountrySelect';
 
 export interface BaseSettingsProps { }
 
+// .ito - level up the props
 const BaseSettings: FC<BaseSettingsProps> = () => {
+  const system = 'IPFS'
+  const currency = 'RIF'
+  const [availableSize, setAvailableSize] = useState(1)
+
+  const onSizeChange = ({ target: { value } }) => {
+    setAvailableSize(validatedNumber(Number(value)))
+  }
+
   return (
     <>
       <Grid item xs={12} md={6}>
         <TextField
           select fullWidth required label='System'
           id="system-select"
-          value='IPFS'
+          value={system}
         >
           <MenuItem value='IPFS'>IPFS</MenuItem>
-          <MenuItem value='ASDF'>ASDF</MenuItem>
-          <MenuItem value='ZXCVB'>ZXCVB</MenuItem>
         </TextField>
       </Grid>
       <Grid item xs={12} md={6}>
@@ -25,8 +40,11 @@ const BaseSettings: FC<BaseSettingsProps> = () => {
             <TextField
               required
               fullWidth
+              type='number'
               label="Available Size"
               id="available-size"
+              value={availableSize.toString()}
+              onChange={onSizeChange}
               InputProps={{
                 endAdornment: (
                   <InputAdornment position="end">
@@ -47,17 +65,7 @@ const BaseSettings: FC<BaseSettingsProps> = () => {
       <Grid item xs={12} md={6}>
         <Grid container spacing={1} alignItems='center'>
           <Grid item xs={10}>
-            <TextField
-              select fullWidth label='Location'
-              id="location-select"
-              value='ARG'
-            >
-              <MenuItem value='ARG'>ARG</MenuItem>
-              <MenuItem value='SVK'>SVK</MenuItem>
-              <MenuItem value='UYU'>UYU</MenuItem>
-              <MenuItem value='CZE'>CZE</MenuItem>
-              <MenuItem value='UKR'>UKR</MenuItem>
-            </TextField>
+            <CountrySelect />
           </Grid>
           <Grid item xs={2}>
             <Tooltip title='Regulations to use the uploaded content are directly associated to the location.'>
@@ -70,10 +78,9 @@ const BaseSettings: FC<BaseSettingsProps> = () => {
         <TextField
           select fullWidth label='Currency'
           id="currency-select"
-          value='RIF'
+          value={currency}
         >
           <MenuItem value='RIF'>RIF</MenuItem>
-          <MenuItem value='USD'>USD</MenuItem>
         </TextField>
       </Grid>
     </>
--- src/components/organisms/storage/listing/PlanItems.tsx
+++ src/components/organisms/storage/listing/PlanItems.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useState, useEffect } from 'react'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import { colors } from '@rsksmart/rif-ui'
@@ -30,7 +30,11 @@ const PlanItems: FC<PlanItemsProps> = props => {
   const [plansCounter, setPlansCounter] = useState(0)
   const [availableMonths, setAvailableMonths] = useState([1, 2, 3])
   // const [availableMonths, setAvailableMonths] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
-  const [defaultNewMonth, setDefaultNewMonth] = useState(availableMonths[0])
+  const [suggestedMonth, setSuggestedMonth] = useState(availableMonths[0])
+
+  useEffect(() => {
+    if (availableMonths.length > 0) setSuggestedMonth(availableMonths[0])
+  }, [availableMonths])
 
   const onPlanAdded = (plan: StoragePlan) => {
     const newPlan = {
@@ -38,42 +42,52 @@ const PlanItems: FC<PlanItemsProps> = props => {
       _internalId: plansCounter
     }
     setPlansCounter(plansCounter + 1)
+    // add to the currentPlans
     setCurrentPlans([newPlan, ...currentPlans])
     // remove month from the available options
     setAvailableMonths([...availableMonths.filter(x => x !== plan.monthsDuration)])
-    setDefaultNewMonth(availableMonths[0])
   }
 
   const onItemRemoved = (plan: StoragePlan) => {
     // add the month to the available options
     setAvailableMonths([...availableMonths, plan.monthsDuration])
+    // remove from the currentPlans
     setCurrentPlans([...currentPlans.filter(x => x._internalId !== plan._internalId)])
-    setDefaultNewMonth(availableMonths[0])
   }
 
   return (
     <>
       {/* SET PLAN PRICES */}
-      {availableMonths.length > 0 && (<Grid item xs={12}>
-        <Typography color='secondary' variant='subtitle1'>SET PLAN PRICES</Typography>
-        <EditablePlan initialMonth={availableMonths[0]} availableMonths={availableMonths} onPlanAdded={onPlanAdded} />
-      </Grid>)}
+      {
+        availableMonths.length > 0 &&
+        (
+          <Grid item xs={12}>
+            <Typography color='secondary' variant='subtitle1'>SET PLAN PRICES</Typography>
+            <EditablePlan suggestedMonth={suggestedMonth} availableMonths={availableMonths} onPlanAdded={onPlanAdded} />
+          </Grid>
+        )
+      }
       {/* STORAGE PLANS */}
-      <Grid item xs={12}>
-        <Typography gutterBottom color='secondary' variant='subtitle1'>STORAGE PLANS</Typography>
-        <Grid className={classes.storagePlans} container spacing={2}>
-          {
-            currentPlans.sort((a, b) => a.monthsDuration - b.monthsDuration).map((p: StoragePlan) => (
-              <PlanItem
-                key={p._internalId}
-                monthlyDuration={mayBePluralize(p.monthsDuration, 'month')}
-                rifPrice={p.pricePerGb}
-                onItemRemoved={() => onItemRemoved(p)}
-              />
-            ))
-          }
-        </Grid>
-      </Grid>
+      {
+        currentPlans.length > 0 &&
+        (
+          <Grid item xs={12}>
+            <Typography gutterBottom color='secondary' variant='subtitle1'>STORAGE PLANS</Typography>
+            <Grid className={classes.storagePlans} container spacing={2}>
+              {
+                currentPlans.sort((a, b) => a.monthsDuration - b.monthsDuration).map((p: StoragePlan) => (
+                  <PlanItem
+                    key={p._internalId}
+                    monthlyDuration={mayBePluralize(p.monthsDuration, 'month')}
+                    rifPrice={p.pricePerGb}
+                    onItemRemoved={() => onItemRemoved(p)}
+                  />
+                ))
+              }
+            </Grid>
+          </Grid>
+        )
+      }
     </>
   )
 }
