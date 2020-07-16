83a4470465f86e5f077b793147e4fef09b52ec10
Alvaro Fariña <farinalvaro@gmail.com>
2020-07-16 12:29:37 -0300

    feat (styles): improved styles and added min to number fields

--- src/components/molecules/storage/listing/PlanItem.tsx
+++ src/components/molecules/storage/listing/PlanItem.tsx
@@ -27,9 +27,6 @@ const useStyles = makeStyles((theme: Theme) => ({
   }
 }))
 
-// .ito - 2 -> see how to convert a plan item to an editablePlan and act upon that, may be 
-// tal vez tenga que pasar a ser un componente como de alto nivel que maneja cuando mostrar un componente u otro
-// y al clickear editar haya tambien que agregar un save o check button
 const PlanItem: FC<PlanItemProps> = ({ monthlyDuration, rifPrice, onItemRemoved }) => {
   const classes = useStyles()
 
@@ -88,11 +85,11 @@ const PlanItem: FC<PlanItemProps> = ({ monthlyDuration, rifPrice, onItemRemoved 
       </Grid>
       <Grid item xs={2}>
         <Grid container direction='row'>
+          <IconButton color='primary'>
+            <EditIcon />
+          </IconButton>
           <IconButton onClick={onItemRemoved} color='primary'>
             <ClearIcon />
-          </IconButton>
-          <IconButton color="primary">
-            <EditIcon />
           </IconButton>
         </Grid>
       </Grid>
