01d70d99ac4e326b91e7147c292ff9e7e84eb0bf
Alvaro Fariña <farinalvaro@gmail.com>
2020-07-16 16:59:52 -0300

    fix: rebasing fix

--- src/models/marketItems/StorageItem.ts
+++ src/models/marketItems/StorageItem.ts
@@ -16,22 +16,3 @@ export interface StoragePlan {
   monthsDuration: number
 }
 
-// // export interface StorageFilterIface extends MarketFilterIface {
-
-// // }
-
-export class StorageItem implements StorageItem {
-    provider!: string;
-
-//     size!: number;
-
-//     contractLengthMonths!: number;
-
-//     pricePerMonth!: number;
-
-//     currency!: string;
-
-//     priceUsd!: number;
-
-//     id!: string;
-// }
