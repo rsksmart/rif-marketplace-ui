14ae2f554ad00a8d91328504c6ba34a7a82a9b55
Juraj Piar <juraj@iovlabs.org>
2020-07-21 14:16:33 +0100

    feat: setting up auth service

--- src/api/rif-marketplace-cache/config.ts
+++ src/api/rif-marketplace-cache/config.ts
@@ -1,11 +1,15 @@
 import feathers from '@feathersjs/feathers'
 import socketio from '@feathersjs/socketio-client'
 import io from 'socket.io-client'
+import auth from '@feathersjs/authentication-client';
 
 const CACHE_ADDRESS = process.env.REACT_APP_CACHE_ADDR || 'http://localhost:3030'
 
 const socket = io(CACHE_ADDRESS)
 const client = feathers()
 client.configure(socketio(socket))
+client.configure(auth({
+
+}))
 
 export default client
