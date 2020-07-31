95b677aa3888225e387a3ebc01abd1e9aaac9557
Alvaro Fariña <farinalvaro@gmail.com>
2020-07-31 15:29:47 -0300

    feat: adds structure needed to communicate with storage contract

--- src/contracts/config.ts
+++ src/contracts/config.ts
@@ -1,9 +1,12 @@
 import networkConfig from 'config'
 
 const {
-  contractAddresses: { marketplace, rif, rnsDotRskOwner },
+  contractAddresses: {
+    marketplace, rif, rnsDotRskOwner, storageManager,
+  },
 } = networkConfig
 
 export const marketPlaceAddress = marketplace.toLowerCase()
 export const rifTokenAddress = rif.toLowerCase()
 export const rnsAddress = rnsDotRskOwner.toLowerCase()
+export const storageAddress = storageManager.toLowerCase()
