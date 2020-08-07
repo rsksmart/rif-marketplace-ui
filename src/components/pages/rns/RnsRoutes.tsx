7b11533202cd14ba472a246e6b839f3a6ded1ccc
Juraj Piar <juraj@iovlabs.org>
2020-08-07 17:43:46 +0100

    feat: adds route specific providers for rns

--- src/components/pages/rns/RnsRoutes.tsx
+++ src/components/pages/rns/RnsRoutes.tsx
@@ -10,6 +10,39 @@ import {
   SellDomainsListPage, DomainListed, DomainPurchased, CancelDomainCheckoutPage, DomainCanceled,
 } from './index'
 import RnsLandingPage from './RnsLandingPage'
+import { RnsDomainsStoreProvider } from 'store/Market/rns/DomainsStore'
+import { RnsOffersStoreProvider } from 'store/Market/rns/OffersStore'
+import { RnsSoldStoreProvider } from 'store/Market/rns/SoldStore'
+
+
+const OffersRoutes = () => {
+
+  return (
+    <Switch>
+      <RnsOffersStoreProvider>
+        <Route exact path={ROUTES.DOMAINS.BUY} component={DomainOffersPage} />
+        <Route exact path={ROUTES.DOMAINS.CHECKOUT.BUY} component={DomainOffersCheckoutPage} />
+        <Route exact path={ROUTES.DOMAINS.DONE.BUY} component={DomainPurchased} />
+      </RnsOffersStoreProvider>
+    </Switch>
+  )
+}
+
+const DomainsRoutes = () => {
+
+  return (
+    <Switch>
+      <RnsDomainsStoreProvider>
+        <RnsSoldStoreProvider>
+          <Route exact path={ROUTES.DOMAINS.SELL} component={SellDomainsListPage} />
+        </RnsSoldStoreProvider>
+        <Route exact path={ROUTES.DOMAINS.CHECKOUT.SELL} component={DomainsCheckoutPage} />
+        <Route exact path={ROUTES.DOMAINS.DONE.SELL} component={DomainListed} />
+        <Route exact path={ROUTES.DOMAINS.CHECKOUT.CANCEL} component={CancelDomainCheckoutPage} />
+      </RnsDomainsStoreProvider>
+    </Switch>
+  )
+}
 
 const RnsRoutes = () => {
   const { services } = networkConfig
@@ -19,13 +52,8 @@ const RnsRoutes = () => {
     return (
       <Switch>
         <Redirect exact from={ROUTES.DOMAINS.BASE} to={ROUTES.DOMAINS.BUY} />
-        <Route exact path={ROUTES.DOMAINS.BUY} component={DomainOffersPage} />
-        <Route exact path={ROUTES.DOMAINS.CHECKOUT.BUY} component={DomainOffersCheckoutPage} />
-        <Route exact path={ROUTES.DOMAINS.DONE.BUY} component={DomainPurchased} />
-        <Route exact path={ROUTES.DOMAINS.SELL} component={SellDomainsListPage} />
-        <Route exact path={ROUTES.DOMAINS.CHECKOUT.SELL} component={DomainsCheckoutPage} />
-        <Route exact path={ROUTES.DOMAINS.DONE.SELL} component={DomainListed} />
-        <Route exact path={ROUTES.DOMAINS.CHECKOUT.CANCEL} component={CancelDomainCheckoutPage} />
+        <Route path={ROUTES.DOMAINS.BUY} component={OffersRoutes} />
+        <Route path={ROUTES.DOMAINS.SELL} component={DomainsRoutes} />
         <Route exact path={ROUTES.DOMAINS.DONE.CANCEL} component={DomainCanceled} />
         <Route component={NotFound} />
       </Switch>
