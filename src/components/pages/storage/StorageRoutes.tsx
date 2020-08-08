98e95a308efddd65c839aa19dc30eacd037c0d10
Juraj Piar <juraj@iovlabs.org>
2020-08-08 16:56:21 +0100

    feat: adjust storage routing

--- src/components/pages/storage/StorageRoutes.tsx
+++ src/components/pages/storage/StorageRoutes.tsx
@@ -11,6 +11,21 @@ import StorageOffersPage from './buy/StorageOffersPage'
 import StorageListingPage from './sell/StorageListingPage'
 import StorageOfferListed from './sell/StorageOfferListed'
 
+const BuyingRoutes = () => (
+  <Switch>
+    <Route exact path={ROUTES.STORAGE.BUY.BASE} component={StorageOffersPage} />
+  </Switch>
+)
+
+const SellingRoutes = () => (
+  <Switch>
+    <StorageListingStoreProvider>
+      <Route exact path={ROUTES.STORAGE.SELL.BASE} component={StorageListingPage} />
+    </StorageListingStoreProvider>
+    <Route exact path={ROUTES.STORAGE.SELL.DONE} component={StorageOffersPage} />
+  </Switch>
+)
+
 const StorageRoutes = () => {
   const { services } = networkConfig
   const storageEnabled = services && (services as string[]).includes('storage')
@@ -18,13 +33,10 @@ const StorageRoutes = () => {
   if (storageEnabled) {
     return (
       <Switch>
-        <Redirect exact from={ROUTES.STORAGE.BASE} to={ROUTES.STORAGE.BUY} />
+        <Redirect exact from={ROUTES.STORAGE.BASE} to={ROUTES.STORAGE.BUY.BASE} />
         <Route exact path={ROUTES.STORAGE.BASE} component={StorageLandingPage} />
-        <Route exact path={ROUTES.STORAGE.BUY} component={StorageOffersPage} />
-        <Route exact path={ROUTES.STORAGE.DONE.SELL} component={StorageOfferListed} />
-        <StorageListingStoreProvider>
-          <Route exact path={ROUTES.STORAGE.SELL} component={StorageListingPage} />
-        </StorageListingStoreProvider>
+        <Route path={ROUTES.STORAGE.BUY.BASE} component={BuyingRoutes} />
+        <Route path={ROUTES.STORAGE.SELL.BASE} component={SellingRoutes} />
         <Route component={NotFound} />
       </Switch>
     )
