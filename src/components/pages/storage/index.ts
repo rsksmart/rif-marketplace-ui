b962e0ff95eb82850b90fc7302119dd78e85fb7a
Alvaro Fariña <farinalvaro@gmail.com>
2020-08-26 17:59:03 -0300

    feat: adds storage my offers page with only staking card

--- src/components/pages/storage/index.ts
+++ src/components/pages/storage/index.ts
@@ -1,5 +1,11 @@
 import StorageListingPage from './sell/StorageListingPage'
 import StorageOffersPage from './buy/StorageOffersPage'
 import StorageLandingPage from './StorageLandingPage'
+import StorageMyOffersPage from './sell/StorageMyOffersPage'
 
-export { StorageListingPage, StorageLandingPage, StorageOffersPage }
+export {
+  StorageListingPage,
+  StorageLandingPage,
+  StorageOffersPage,
+  StorageMyOffersPage,
+}
