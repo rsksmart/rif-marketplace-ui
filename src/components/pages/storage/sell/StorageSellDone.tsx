04657a27bbbafcf751788837d8d71f2011c8a278
Alvaro Fariña <farinalvaro@gmail.com>
2020-08-28 12:03:50 -0300

    refactor: renames storage/listing to storage/sell

--- src/components/pages/storage/sell/StorageSellDone.tsx
+++ src/components/pages/storage/sell/StorageSellDone.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import TxCompletePageTemplate from 'components/templates/TxCompletePageTemplate'
 import { JobDoneBox } from 'components/molecules'
 
-const StorageOfferListed = () => (
+const StorageSellDone = () => (
   <TxCompletePageTemplate>
     <JobDoneBox text="Your offer has been listed." />
   </TxCompletePageTemplate>
 )
 
-export default StorageOfferListed
+export default StorageSellDone
