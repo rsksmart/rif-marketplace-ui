867c07516ba09afaa49b84373913938124384867
Alvaro Fariña <farinalvaro@gmail.com>
2020-09-02 17:56:56 -0300

    refactor(styles): fixes styles to match new design

--- src/components/templates/storage/sell/PlanItemBaseFormTemplate.tsx
+++ src/components/templates/storage/sell/PlanItemBaseFormTemplate.tsx
@@ -5,7 +5,6 @@ import TextField from '@material-ui/core/TextField'
 import MenuItem from '@material-ui/core/MenuItem'
 import InputAdornment from '@material-ui/core/InputAdornment'
 import { colors, validatedNumber } from '@rsksmart/rif-ui'
-import { makeStyles } from '@material-ui/core'
 import { TimePeriodEnum } from 'context/Services/storage/interfaces'
 import LabelWithValue from 'components/atoms/LabelWithValue'
 
@@ -21,21 +20,11 @@ export interface PlanItemBaseFormTemplateProps {
   fiatDisplayName: string
 }
 
-const useStyles = makeStyles(() => ({
-  subscriptionCreatorPrice: {
-    // .ito
-    // backgroundColor: colors.gray1,
-    // borderRadius: '5px',
-  },
-}))
-
 const PlanItemBaseFormTemplate: FC<PlanItemBaseFormTemplateProps> = (props) => {
   const {
     onPeriodChange, price, onPriceChange, fiatPrice, fiatDisplayName,
     currency, periodOptions, selectedPeriod, availablePeriods,
   } = props
-
-  const classes = useStyles()
 
   const handleOnPeriodChange = ({ target: { value } }) => {
     onPeriodChange(value)
@@ -46,14 +35,13 @@ const PlanItemBaseFormTemplate: FC<PlanItemBaseFormTemplateProps> = (props) => {
   }
 
   return (
-    <>
-      {/* available months */}
+    <Grid container spacing={2}>
       <Grid item xs={12} md={3}>
         <TextField
           select
           fullWidth
           required
-          label="Subscription Period"
+          label="Period"
           id="subscription-period-select"
           value={selectedPeriod}
           onChange={handleOnPeriodChange}
@@ -75,8 +63,7 @@ const PlanItemBaseFormTemplate: FC<PlanItemBaseFormTemplateProps> = (props) => {
           }
         </TextField>
       </Grid>
-      {/* price */}
-      <Grid item xs={10} md={3}>
+      <Grid item xs={12} md={3}>
         <TextField
           select
           fullWidth
@@ -86,18 +73,17 @@ const PlanItemBaseFormTemplate: FC<PlanItemBaseFormTemplateProps> = (props) => {
           InputProps={{
             style: { textAlign: 'center' },
           }}
-        // onChange={onCurrencyChange}
+        // TODO: handle on change
         >
           <MenuItem value="RBTC">RBTC</MenuItem>
           <MenuItem value="RIF">RIF</MenuItem>
         </TextField>
       </Grid>
-      <Grid item xs={10} md={3}>
+      <Grid item xs={12} md={6}>
         <Grid
-          className={classes.subscriptionCreatorPrice}
           container
           spacing={1}
-          alignItems="flex-end"
+          alignItems="center"
         >
           <Grid item xs={6}>
             <TextField
@@ -125,28 +111,10 @@ const PlanItemBaseFormTemplate: FC<PlanItemBaseFormTemplateProps> = (props) => {
           </Grid>
           <Grid item xs={6}>
             <LabelWithValue label={fiatPrice} value={fiatDisplayName} />
-            {/* <TextField
-              disabled
-              fullWidth
-              label=" "
-              id="price-gb-usd"
-              value={fiatPrice}
-              InputProps={{
-                inputProps: {
-                  style: { textAlign: 'center' },
-                },
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <Typography variant="caption" color="secondary">{fiatDisplayName}</Typography>
-                  </InputAdornment>
-                ),
-                style: { color: colors.gray4 },
-              }}
-            /> */}
           </Grid>
         </Grid>
       </Grid>
-    </>
+    </Grid>
   )
 }
 
