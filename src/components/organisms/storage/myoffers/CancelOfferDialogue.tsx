bf01bbf297e748f471bfefc197e51a44c5378a45
Alvaro Fariña <farinalvaro@gmail.com>
2020-09-09 17:43:54 -0300

    fix(styles): moves to classes and explicitly set container

--- src/components/organisms/storage/myoffers/CancelOfferDialogue.tsx
+++ src/components/organisms/storage/myoffers/CancelOfferDialogue.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import { Button, ModalDialogue } from '@rsksmart/rif-ui'
+import { makeStyles, Theme } from '@material-ui/core'
 
 export interface CancelOfferDialogueProps {
   open: boolean
@@ -9,10 +10,17 @@ export interface CancelOfferDialogueProps {
   onConfirmCancel: () => void
 }
 
+const useStyles = makeStyles((theme: Theme) => ({
+  buttonNo: {
+    marginRight: theme.spacing(2),
+  },
+}))
+
 const CancelOfferDialogue: FC<CancelOfferDialogueProps> = ({ open, onClose, onConfirmCancel }) => {
+  const classes = useStyles()
   const actions = (
-    <Grid justify="flex-end">
-      <Button style={{ marginRight: 8 }} color="primary" rounded variant="outlined" onClick={onClose}>No</Button>
+    <Grid container justify="center">
+      <Button className={classes.buttonNo} color="primary" rounded variant="outlined" onClick={onClose}>No</Button>
       <Button color="primary" rounded variant="contained" onClick={onConfirmCancel}>Yes</Button>
     </Grid>
   )
@@ -23,7 +31,7 @@ const CancelOfferDialogue: FC<CancelOfferDialogueProps> = ({ open, onClose, onCo
       title="Are you sure you want to cancel this offer?"
       footer={actions}
     >
-      <Grid justify="center">
+      <Grid container justify="center">
         <Typography align="center" color="secondary">
           The offer will be cancelled inmediately but your active contracts will run until the expiration date.
         </Typography>
