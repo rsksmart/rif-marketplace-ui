7b462d7d827f758ff6cb6818c31db8e69d86fcc6
Alvaro Fariña <farinalvaro@gmail.com>
2020-09-14 16:22:56 -0300

    feat: adds card not any offer yet

--- src/components/organisms/storage/myoffers/OffersList.tsx
+++ src/components/organisms/storage/myoffers/OffersList.tsx
@@ -4,6 +4,7 @@ import ExpandableOffer from 'components/organisms/storage/myoffers/ExpandableOff
 import { StorageOffer } from 'models/marketItems/StorageItem'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
+import RoundedCard from 'components/atoms/RoundedCard'
 
 export interface OffersListProps {
   items: StorageOffer[]
@@ -13,14 +14,28 @@ const useStyles = makeStyles((theme: Theme) => ({
   expandableOffer: {
     margin: theme.spacing(2, 0),
   },
+  noOffers: {
+    padding: theme.spacing(4),
+  },
 }))
 
 const OffersList: FC<OffersListProps> = ({ items }) => {
   const classes = useStyles()
+
+  if (!items.length) {
+    return (
+      <RoundedCard color="secondary">
+        <Grid container className={classes.noOffers}>
+          <Typography align="center" color="secondary">No offers created yet</Typography>
+        </Grid>
+      </RoundedCard>
+    )
+  }
+
   return (
     <Grid container>
       {
-        !!items.length && items.map((storageOffer: StorageOffer, i) => (
+        items.map((storageOffer: StorageOffer, i) => (
           <ExpandableOffer
             key={storageOffer.id}
             className={classes.expandableOffer}
@@ -29,9 +44,6 @@ const OffersList: FC<OffersListProps> = ({ items }) => {
             initiallyExpanded={!i}
           />
         ))
-      }
-      {
-        !items.length && <Typography color="secondary">No offers created yet</Typography>
       }
     </Grid>
   )
