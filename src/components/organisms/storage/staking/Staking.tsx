1d9216af26c177e0b3ce0584fccd65bc6e56e1b9
Alvaro Fariña <farinalvaro@gmail.com>
2020-10-07 17:55:03 -0300

    feat: fades in a test element on click

--- src/components/organisms/storage/staking/Staking.tsx
+++ src/components/organisms/storage/staking/Staking.tsx
@@ -1,7 +1,8 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import Fab from '@material-ui/core/Fab'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 import AddIcon from '@material-ui/icons/Add'
+import { Fade, Paper } from '@material-ui/core'
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
   root: {
@@ -9,13 +10,35 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
     display: 'flex',
     justifyContent: 'flex-end',
   },
+  paper: {
+    margin: theme.spacing(1),
+  },
+  svg: {
+    width: 100,
+    height: 100,
+  },
+  polygon: {
+    fill: theme.palette.common.white,
+    stroke: theme.palette.divider,
+    strokeWidth: 1,
+  },
 }))
 
 const Staking: FC<{}> = () => {
   const classes = useStyles()
+  const [checked, setChecked] = useState(false)
   return (
     <div className={classes.root}>
-      <Fab color="primary" aria-label="staking">
+      <Fade in={checked}>
+        <Paper elevation={4} className={classes.paper}>
+          <svg className={classes.svg}>
+            <polygon points="0,100 50,00, 100,100" className={classes.polygon} />
+          </svg>
+        </Paper>
+      </Fade>
+
+      <Fab color="primary" aria-label="staking" onClick={() => setChecked(!checked)}>
+
         <AddIcon />
       </Fab>
     </div>
