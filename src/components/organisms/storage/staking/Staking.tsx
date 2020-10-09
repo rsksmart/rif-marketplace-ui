2fc18b3647ce9f2c2d736c62666df7785e565564
Alvaro Fariña <farinalvaro@gmail.com>
2020-10-09 11:16:22 -0300

    refactor: polishes straking component

--- src/components/organisms/storage/staking/Staking.tsx
+++ src/components/organisms/storage/staking/Staking.tsx
@@ -2,7 +2,9 @@ import React, { FC, useState } from 'react'
 import Fab from '@material-ui/core/Fab'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 import AddIcon from '@material-ui/icons/Add'
-import { Grid, Grow, Typography } from '@material-ui/core'
+import {
+  Box, Grid, Grow, Typography,
+} from '@material-ui/core'
 import { Button, colors } from '@rsksmart/rif-ui'
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
@@ -11,57 +13,54 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
     display: 'flex',
     justifyContent: 'flex-end',
   },
-  paper: {
-    margin: theme.spacing(1),
+  infoContainer: {
+    border: `${colors.primary} 1px solid`,
+    maxWidth: '700px',
+    alignItems: 'center',
+    borderRadius: '50px 0px 0px 50px',
+    paddingRight: '40px',
   },
-  svg: {
-    width: 100,
-    height: 100,
+  infoColumn: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
   },
-  polygon: {
-    fill: theme.palette.common.white,
-    stroke: theme.palette.divider,
-    strokeWidth: 1,
+  stakingIcon: {
+    height: '80px',
+    minWidth: '80px',
+    marginLeft: '-40px',
   },
 }))
 
 const Staking: FC<{}> = () => {
   const classes = useStyles()
   const [expanded, setExpanded] = useState(true)
+
+  const handleExpandClick = () => setExpanded((exp) => !exp)
+
   return (
     <div className={classes.root}>
       <Grow in={expanded}>
         <Grid
           container
-          style={{
-            border: `${colors.primary} 1px solid`,
-            maxWidth: '700px',
-            alignItems: 'center',
-            borderRadius: '50px 0px 0px 50px',
-            paddingRight: '40px',
-          }}
+          className={classes.infoContainer}
         >
           <Grid
             item
             xs={4}
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              flexDirection: 'column',
-            }}
+            className={classes.infoColumn}
           >
-            <Typography style={{ color: colors.gray5 }}>
-              BALANCE
+            <Typography component="div" color="secondary">
+              <Box fontWeight="fontWeightRegular">
+                BALANCE
+              </Box>
             </Typography>
             <Typography color="primary" variant="h6">2048 RIF</Typography>
           </Grid>
           <Grid
             item
             xs={4}
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-            }}
+            className={classes.infoColumn}
           >
             <Button color="primary" rounded variant="outlined">
               Add funds
@@ -70,10 +69,7 @@ const Staking: FC<{}> = () => {
           <Grid
             item
             xs={4}
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-            }}
+            className={classes.infoColumn}
           >
             <Button rounded variant="outlined">
               Withdraw funds
@@ -82,14 +78,10 @@ const Staking: FC<{}> = () => {
         </Grid>
       </Grow>
       <Fab
+        className={classes.stakingIcon}
         color="primary"
         aria-label="staking"
-        onClick={() => setExpanded(!expanded)}
-        style={{
-          height: '80px',
-          minWidth: '80px',
-          marginLeft: '-40px',
-        }}
+        onClick={handleExpandClick}
       >
         <AddIcon />
       </Fab>
