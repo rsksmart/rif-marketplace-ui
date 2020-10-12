1245acd5f0f9d2612d29c0cf89c36bf308956367
Juraj Piar <juraj@iovlabs.org>
2020-10-12 15:45:46 +0100

    refactor: extracts itemcreation fn - adds style to title

--- src/components/pages/storage/myPurchases/Page.tsx
+++ src/components/pages/storage/myPurchases/Page.tsx
@@ -1,18 +1,26 @@
 import React, { FC, useContext } from 'react'
 import {
-  Grid, TableContainer, Typography,
+  makeStyles, TableContainer, Typography,
 } from '@material-ui/core'
-import ItemWUnit from 'components/atoms/ItemWUnit'
 import RoundedCard from 'components/atoms/RoundedCard'
-import { AddressItem, CombinedPriceCell, SelectRowButton } from 'components/molecules'
 import CenteredPageTemplate from 'components/templates/CenteredPageTemplate'
 import Marketplace from 'components/templates/marketplace/Marketplace'
 import MarketContext from 'context/Market/MarketContext'
 import AgreementsContext, { AgreementContextProps } from 'context/Services/storage/agreements'
-import { Agreement } from 'models/marketItems/StorageItem'
-import { UNIT_PREFIX_POW2 } from 'utils/utils'
+import GridColumn from 'components/atoms/GridColumn'
+import GridItem from 'components/atoms/GridItem'
+import { theme } from '@rsksmart/rif-ui'
+import createItemFields from './utils'
+
+const useTitleStyles = makeStyles(() => ({
+  root: {
+    marginBlockStart: `${theme.spacing(2)}px`,
+    marginInlineStart: `${theme.spacing(2)}px`,
+  },
+}))
 
 const MyStoragePurchases: FC = () => {
+  const titleStyleClass = useTitleStyles()
   const {
     state: {
       agreements,
@@ -38,70 +46,33 @@ const MyStoragePurchases: FC = () => {
     view: '',
   }
 
-  type Fields = {
-      [K in keyof typeof headers]: string | JSX.Element
-  }
-
-  const items: Fields[] = agreements.map(({
-    provider,
-    monthlyFee,
-    renewalDate,
-    paymentToken,
-    size,
-    subscriptionPeriod,
-    title,
-    id,
-  }: Agreement) => {
-    const currency = crypto[paymentToken]
-
-    return {
-      id,
-      title: <AddressItem value={title || id} />,
-      contentSize: <ItemWUnit
-        type="mediumPrimary"
-        value={size.div(UNIT_PREFIX_POW2.KILO).toPrecision(2)}
-        unit="GB"
-      />,
-      monthlyFee: <CombinedPriceCell
-        price={monthlyFee.toPrecision(2)}
-        priceFiat={currency && monthlyFee.times(currency.rate).toPrecision(3)}
-        currency={currency && currency.displayName}
-        currencyFiat={currentFiat.displayName}
-        divider=" "
-      />,
-      provider: <AddressItem value={provider} />,
-      renewalDate: renewalDate.toLocaleDateString(),
-      subscriptionType: subscriptionPeriod,
-      renew:
-  <SelectRowButton
-    id={id}
-    handleSelect={(): void => undefined}
-  >
-    Renew
-  </SelectRowButton>,
-      view:
-  <SelectRowButton
-    id={id}
-    handleSelect={(): void => undefined}
-  >
-    View
-  </SelectRowButton>,
-    }
-  })
+  const items = createItemFields(agreements, crypto, currentFiat)
 
   return (
     <CenteredPageTemplate>
       <RoundedCard color="secondary">
-        <Grid container>
-          <Typography gutterBottom color="primary" variant="subtitle1">Active contracts</Typography>
-          <TableContainer>
-            <Marketplace
-              headers={headers}
-              isLoading={false}
-              items={items}
-            />
-          </TableContainer>
-        </Grid>
+        <GridColumn>
+          <GridItem>
+            <Typography
+              gutterBottom
+              color="primary"
+              variant="subtitle1"
+              classes={titleStyleClass}
+            >
+              Active contracts
+
+            </Typography>
+          </GridItem>
+          <GridItem>
+            <TableContainer>
+              <Marketplace
+                headers={headers}
+                isLoading={false}
+                items={items}
+              />
+            </TableContainer>
+          </GridItem>
+        </GridColumn>
       </RoundedCard>
 
     </CenteredPageTemplate>
