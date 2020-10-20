539e6520be1775aa4bb25ef4b69226bb74205489
Juraj Piar <juraj@iovlabs.org>
2020-10-20 16:34:45 +0100

    feat: adds data to the view

--- src/components/molecules/AddressItem.tsx
+++ src/components/molecules/AddressItem.tsx
@@ -8,10 +8,10 @@ export interface AddressItemProps {
 
 const AddressItem: FC<AddressItemProps> = ({ pretext, value }) => {
   const displayElement = (
-    <p>
+    <>
       {pretext && `${pretext} (${shortenString(value)})`}
       {!pretext && shortenString(value)}
-    </p>
+    </>
   )
   return <CopyTextTooltip fullText={value} displayElement={displayElement} />
 }
