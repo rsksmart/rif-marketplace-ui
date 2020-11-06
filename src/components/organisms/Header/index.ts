71e959b17466c09e9d8343b3ae4059eecf260a2b
Juraj Piar <juraj@iovlabs.org>
2020-11-06 21:01:12 +0000

    feat(notifications): adds notif mapping to mesasge

--- src/components/organisms/Header/index.ts
+++
@@ -1,4 +0,0 @@
-import Header from './Header'
-import HeaderTongue from './HeaderTongue'
-
-export { Header, HeaderTongue }
--- src/context/Services/notifications/interfaces.ts
+++ src/context/Services/notifications/interfaces.ts
@@ -1,21 +1,28 @@
 import { serviceAddress } from 'api/rif-marketplace-cache/notifications'
-import { NotificationsItem } from 'api/rif-marketplace-cache/notifications/interfaces'
+import { NotificationItem } from 'api/rif-marketplace-cache/notifications/interfaces'
 import { ContextProps, ContextState } from 'context/storeUtils/interfaces'
 
 // STATE
 export type ContextName = typeof serviceAddress
+
+export type Notifications = NotificationItem[]
+
 export type State = ContextState & {
-    notifications: NotificationsItem[]
+    notifications: Notifications
 }
 
 // PAYLOAD
+type Payload = Notifications
 
 // ACTIONS
 export type Action = ({
-    type: 'NOOP'
+    type: 'SET_NOTIFICATIONS'
+    payload: Payload
 })
 
-export type ActionFunctions = {}
+export type ActionFunctions = {
+    SET_NOTIFICATIONS: (state: State, payload: Payload) => State
+}
 
 // PROPS
 export type Props = ContextProps<State, Action>
