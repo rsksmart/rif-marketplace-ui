0bc4cffe71780992f8cf3b09b2b27231f0dc2038
Alvaro Fariña <farinalvaro@gmail.com>
2020-11-12 16:46:16 -0300

    fix: uses agreements id instead of title

--- src/components/organisms/storage/agreements/utils.tsx
+++ src/components/organisms/storage/agreements/utils.tsx
@@ -36,12 +36,12 @@ const getCoreItemFields = (
   currentFiat: MarketFiat,
 ): AgreementView => {
   const {
-    title,
     monthlyFee,
     renewalDate,
     paymentToken,
     size,
     subscriptionPeriod,
+    id,
     dataReference,
   } = agreement
   const currency = crypto[paymentToken]
@@ -62,11 +62,11 @@ const getCoreItemFields = (
       divider=" "
     />
   )
-  const titleValue = <AddressItem value={title || dataReference} />
+  const idValue = <AddressItem value={id} />
 
   return {
     HASH: <AddressItem value={dataReference} />,
-    title: titleValue,
+    title: idValue,
     'PRICE/GB': feeValue,
     AMOUNT: sizeValue,
     'RENEWAL DATE': getShortDateString(renewalDate),
