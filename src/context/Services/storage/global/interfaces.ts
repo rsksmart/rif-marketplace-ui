19b88b2c9776806d4e788731cf8b560e81fde34e
Alvaro Fariña <farinalvaro@gmail.com>
2020-11-16 17:30:56 -0300

    feat(storage): adds ability to check for whitelisted storage providers  - adds storage global context

---
+++ src/context/Services/storage/global/interfaces.ts
@@ -0,0 +1,29 @@
+import { Dispatch } from 'react'
+
+// STATE
+export type State = {
+  isWhitelistedProvider: boolean
+}
+
+// PAYLOAD
+export type IsWhitelistedProviderPayload = {
+  isWhitelistedProvider: boolean
+}
+
+// ACTIONS
+export type Action = (
+  | {
+    type: 'SET_IS_WHITELISTED_PROVIDER'
+    payload: IsWhitelistedProviderPayload
+  }
+)
+
+export type Actions = {
+  SET_IS_WHITELISTED_PROVIDER: (state: State, payload: IsWhitelistedProviderPayload) => State
+}
+
+// PROPS
+export type Props = {
+  state: State
+  dispatch: Dispatch<Action>
+}
