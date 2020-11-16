0be5d321c70d58d6803dd3dd0f57f56ba45c7d69
Juraj <36888576+jurajpiar@users.noreply.github.com>
2020-11-16 14:47:44 +0000

    feat(storage): validates CID hash and prepends /ipfs/ (#487)
    * feat(storage): ensures user inputs valid cid
    
    * feat(storage): adds /ipfs/ prefix to cid
    
    * feat(storage): adds ipfs prefix to cid
    
    * fix: adds custom test environment
    
    * feat: comments out the App.tsx test
    
    * fix: fixes typo
    
    Co-authored-by: Juraj Piar <juraj@iovlabs.org>

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -1,5 +1,21 @@
+import CID from 'cids'
+
 export const isEmpty = (
   text: string | unknown,
 ): boolean => !text || !String(text).trim()
 
-export default {}
+export const validateCID = (
+  cid: string,
+  errorHandle?: (e: Error) => unknown,
+): boolean => {
+  try {
+    CID.validateCID(new CID(cid))
+
+    return true
+  } catch (e) {
+    if (errorHandle) {
+      errorHandle(e)
+    }
+    return false
+  }
+}
