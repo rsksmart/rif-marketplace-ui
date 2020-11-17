e1dfd696af8ebabca65bb834a14d82dcaed9d95a
Nazar Duchak <nazar@iovlabs.org>
2020-11-17 13:05:53 +0200

    feat: update marketplace contract

--- src/contracts/Marketplace.ts
+++ src/contracts/Marketplace.ts
@@ -1,91 +1,64 @@
 import RNSSimplePlacementsV1 from '@rsksmart/rif-marketplace-nfts/RNSSimplePlacementsV1Data.json'
 import Web3 from 'web3'
 import { TransactionReceipt } from 'web3-eth'
-import { Contract } from 'web3-eth-contract'
 import { AbiItem } from 'web3-utils'
+
 import { marketPlaceAddress } from './config'
 import { TransactionOptions } from './interfaces'
-import withWaitForReceipt from './utils'
+import ContractBase from './wrappers/contract-base'
 
 export type MarketplaceContractErrorId = 'contract-marketplace-place' | 'contract-marketplace-unplace' | 'contract-marketplace-getPlacement'
 
-class MarketplaceContract {
+class MarketplaceContract extends ContractBase {
   public static getInstance(web3: Web3): MarketplaceContract {
     if (!MarketplaceContract.instance) {
-      MarketplaceContract.instance = new MarketplaceContract(web3)
+      MarketplaceContract.instance = new MarketplaceContract(
+        web3,
+        new web3.eth.Contract(
+            RNSSimplePlacementsV1.abi as AbiItem[],
+            marketPlaceAddress,
+        ),
+      )
     }
     return MarketplaceContract.instance
   }
 
   private static instance: MarketplaceContract
 
-  private contract: Contract
-
-  private web3: Web3
-
-  private constructor(web3: Web3) {
-    this.contract = new web3.eth.Contract(
-      RNSSimplePlacementsV1.abi as AbiItem[],
-      marketPlaceAddress,
-    )
-    this.web3 = web3
-  }
-
   // Place: Proxy function for placement transaction
-  public place = async (
+  public place(
     tokenId: string,
     rifTokenAddress: string,
     price: string,
-    txOptions: TransactionOptions): Promise<TransactionReceipt> => {
-    // Get gas limit for Placement
-    const { from, gasPrice } = txOptions
-    const estimatedGas = await this.contract.methods.place(
-      tokenId,
-      rifTokenAddress,
-      this.web3.utils.toWei(price),
+    txOptions: TransactionOptions,
+  ): Promise<TransactionReceipt> {
+    const placeTx = this.contract.methods.place(
+      tokenId, rifTokenAddress, this.web3.utils.toWei(price),
     )
-      .estimateGas({ from, gasPrice })
-    const gas = Math.floor(estimatedGas * 1.1)
-
-    // Placement Transaction
-    return new Promise<TransactionReceipt>(
-      () => this.contract.methods.place(
-        tokenId, rifTokenAddress, this.web3.utils.toWei(price),
-      )
-        .send(
-          { from, gas, gasPrice },
-          withWaitForReceipt(this.web3),
-        ),
+    return this._send(
+      placeTx,
+      { ...txOptions, gasMultiplier: 1.1 },
     )
   }
 
   // Unplace: Proxy function for unplacement transaction
-  public unplace = async (
+  public unplace(
     tokenId: string,
-    txOptions: TransactionOptions): Promise<TransactionReceipt> => {
-    // Get gas limit for Unplacement
-    const { from, gasPrice } = txOptions
-    const estimatedGas = await this.contract.methods.unplace(tokenId)
-      .estimateGas({ from, gasPrice })
-    const gas = Math.floor(estimatedGas * 1.1)
-
-    // Unplacement Transaction
-    return new Promise<TransactionReceipt>(
-      () => this.contract.methods.unplace(tokenId)
-        .send(
-          { from, gas, gasPrice },
-          withWaitForReceipt(this.web3),
-        ),
+    txOptions: TransactionOptions,
+  ): Promise<TransactionReceipt> {
+    const unplaceTx = this.contract.methods.unplace(tokenId)
+    return this._send(
+      unplaceTx,
+      { ...txOptions, gasMultiplier: 1.1 },
     )
   }
 
-  public getPlacement = (
+  public getPlacement(
     tokenId: string,
     txOptions: TransactionOptions,
-  ): Promise<Array<string>> => {
+  ): Promise<Array<string>> {
     const { from } = txOptions
-    const placement = this.contract.methods.placement(tokenId).call({ from })
-    return placement
+    return this.contract.methods.placement(tokenId).call({ from })
   }
 }
 
