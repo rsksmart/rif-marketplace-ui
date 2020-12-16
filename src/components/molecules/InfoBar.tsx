4b72ef8098da8eaf1205fb90fb044e6de971f57c
Alvaro Fariña <farinalvaro@gmail.com>
2020-12-16 11:13:26 -0300

    refactor(infobar): makes button optional

--- src/components/molecules/InfoBar.tsx
+++ src/components/molecules/InfoBar.tsx
@@ -1,5 +1,5 @@
 import {
-  Button, ButtonProps, createStyles, makeStyles, Snackbar, Theme, Typography,
+  Button, ButtonProps, createStyles, makeStyles, Snackbar, Typography,
 } from '@material-ui/core'
 import MuiAlert from '@material-ui/lab/Alert'
 import { Severity } from 'models/UIMessage'
@@ -8,12 +8,12 @@ import React, { FC } from 'react'
 export interface InfoBarProps {
   isVisible: boolean
   text: string
-  button: ButtonProps
-  buttonText: string
+  button?: ButtonProps
+  buttonText?: string
   type: Severity
 }
 
-const useStyles = makeStyles((_: Theme) => createStyles({
+const useStyles = makeStyles(() => createStyles({
   alert: {
     alignItems: 'center',
     display: 'flex',
@@ -28,6 +28,7 @@ const InfoBar: FC<InfoBarProps> = ({
   isVisible, text, button, buttonText, type,
 }) => {
   const classes = useStyles()
+  const showButton = Boolean(button) && Boolean(buttonText)
 
   return (
     <Snackbar
@@ -36,7 +37,11 @@ const InfoBar: FC<InfoBarProps> = ({
     >
       <MuiAlert severity={type} className={classes.alert}>
         <Typography display="inline">{text}</Typography>
-        <Button className={classes.button} color="primary" {...button}>{buttonText}</Button>
+        {
+          showButton && (
+            <Button className={classes.button} color="primary" {...button}>{buttonText}</Button>
+          )
+        }
       </MuiAlert>
     </Snackbar>
   )
