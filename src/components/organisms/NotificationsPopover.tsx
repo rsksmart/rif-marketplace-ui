a37544ee003c88702360c9750f834fb539bf144e
Alvaro Fariña <farinalvaro@gmail.com>
2021-01-25 14:44:10 -0300

    feat(notifications): adds no notifications message (#668)
    Co-authored-by: Julian M. Rodriguez <56316686+julianmrodri@users.noreply.github.com>

--- src/components/organisms/NotificationsPopover.tsx
+++ src/components/organisms/NotificationsPopover.tsx
@@ -3,6 +3,7 @@ import Divider from '@material-ui/core/Divider'
 import { PopoverProps } from '@material-ui/core/Popover'
 import { Notifications } from 'context/Services/notifications/interfaces'
 import PopoverCardTemplate from 'components/templates/PopoverCardTemplate'
+import Typography from '@material-ui/core/Typography'
 import NotificationMessage from './NotificationMessage'
 
 type Props = {
@@ -13,27 +14,43 @@ const NotificationsPopover: FC<Props> = ({
   notifications,
   onClose,
   ...popoverProps
-}) => (
-  <PopoverCardTemplate
-    id="notifications-menu"
-    onClose={onClose}
-    cardTitle="Notifications"
-    {...popoverProps}
-  >
-    {
-      Boolean(notifications) && notifications.map((notification, i) => (
-        <div key={
-          notification.payload.timestamp
-          + notification.payload.agreementReference
-        }
-        >
-          {Boolean(i) && <Divider />}
-          <NotificationMessage {...{ notification }} />
-        </div>
-      ))
+}) => {
+  const renderContent = (): JSX.Element => {
+    if (!notifications.length) {
+      return (
+        <Typography variant="caption">
+          No new notifications
+        </Typography>
+      )
     }
-  </PopoverCardTemplate>
-)
+    return (
+      <>
+        {
+          notifications.map((notification, i) => (
+            <div key={
+              notification.payload.timestamp
+              + notification.payload.agreementReference
+            }
+            >
+              {Boolean(i) && <Divider />}
+              <NotificationMessage {...{ notification }} />
+            </div>
+          ))
+        }
+      </>
+    )
+  }
+  return (
+    <PopoverCardTemplate
+      id="notifications-menu"
+      onClose={onClose}
+      cardTitle="Notifications"
+      {...popoverProps}
+    >
+      {renderContent()}
+    </PopoverCardTemplate>
+  )
+}
 
 export default NotificationsPopover
 export type NotificationsPopoverProps = Props
