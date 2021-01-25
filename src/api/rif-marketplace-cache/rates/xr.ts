bc140e75b01b913d945aab96b1177782a2026160
Juraj Piar <juraj.piar@icloud.com>
2021-01-26 00:04:17 +0100

    feat(rns): changes price sort to avgUSDPrice
    feat(rns): sort by fiat
    
    - makes price-filter unit 'USD'

--- src/api/rif-marketplace-cache/rates/xr.ts
+++ src/api/rif-marketplace-cache/rates/xr.ts
@@ -1,6 +1,6 @@
 import { Paginated } from '@feathersjs/feathers'
 import { AbstractAPIService, APIService, isResultPaginated } from 'api/models/apiService'
-import { SupportedTokenSymbol } from 'models/Token'
+import { BaseCurrency, SupportedTokenSymbol } from 'models/Token'
 import { getSupportedTokenByName } from 'utils/tokenUtils'
 import { Modify } from 'utils/typeUtils'
 import client from '../client'
@@ -8,16 +8,58 @@ import client from '../client'
 export type XRServiceAddress = 'rates/v0'
 export const xrServiceAddress: XRServiceAddress = 'rates/v0'
 
-export type SupportedFiat = 'usd' | 'eur' | 'btc' | 'ars' | 'cny' | 'krw' | 'jpy';
+// - juraj
+// move to Model?
+export enum SYSTEM_SUPPORTED_FIAT {
+  usd = 'usd',
+  // (fiatMulC): Enable for fiat multicurrency
+  // eur = 'eur',
+  // ars = 'ars',
+  // cny = 'cny',
+  // krw = 'krw',
+  // jpy = 'jpy',
+}
+
+// - juarj
+// Abstract fiat + crypto type to extract 'btc' from SupportedFiatSymbol, SupportedTokenSymbol
+// export type SupportedFiatSymbol = [
+//   | SYSTEM_SUPPORTED_FIAT.usd
+// (fiatMulC): Enable for fiat multicurrency
+//   | SYSTEM_SUPPORTED_FIAT.eur
+//   | SYSTEM_SUPPORTED_FIAT.ars
+//   | SYSTEM_SUPPORTED_FIAT.cny
+//   | SYSTEM_SUPPORTED_FIAT.krw
+//   | SYSTEM_SUPPORTED_FIAT.jpy
+//   | 'btc' // temporary: get from SupportedTokenSymbol
+// ]
+
+export type SupportedFiatSymbol = 'usd'
+// (fiatMulC): Enable for fiat multicurrency
+// | 'eur' | 'btc' | 'ars' | 'cny' | 'krw' | 'jpy';
+
+// -juraj
+// BaseFiat extend w/ BaseCurrency
+export type BaseFiat = {
+  symbol: SupportedFiatSymbol
+  displayName: string
+}
+export type TokenRecord<T extends BaseFiat> = Record<SupportedFiatSymbol, T>
+
+export const SUPPORTED_FIAT: TokenRecord<BaseFiat> = {
+  usd: {
+    symbol: 'usd',
+    displayName: 'USD',
+  },
+}
 
 export type XRItem = Partial<{
-  [F in SupportedFiat]: number
+  [F in SupportedFiatSymbol]: number
 }> & {
   token: SupportedTokenSymbol
 }
 
 export interface XRFilter {
-  fiatSymbol: SupportedFiat
+  fiatSymbol: SupportedFiatSymbol
 }
 
 export type XRAPIService = Modify<APIService, {
