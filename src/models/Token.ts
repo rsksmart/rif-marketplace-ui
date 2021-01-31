14114cf7ff6f14f92b3578ff5e2f57f5136a48a3
Juraj Piar <juraj.piar@icloud.com>
2021-01-31 17:44:33 +0100

    feat(rns): adapts price filter to use usd

--- src/models/Token.ts
+++ src/models/Token.ts
@@ -2,9 +2,6 @@ import { ZERO_ADDRESS } from 'constants/strings'
 import { rifTokenAddress } from 'contracts/config'
 import { BaseCurrency } from './Currency'
 
-// - juarj
-// create issue:
-//   get SYSTEM_SUPPORTED_SYMBOL from: [config?, ?]
 export enum SYSTEM_SUPPORTED_SYMBOL {
   rif = 'rif',
   rbtc = 'rbtc'
