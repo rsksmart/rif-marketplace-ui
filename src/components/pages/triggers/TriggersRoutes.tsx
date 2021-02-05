8f64273eaeb55fc9556348157ccf166e36457da9
Alvaro Fariña <farinalvaro@gmail.com>
2021-02-05 12:31:45 -0300

    feat(triggers-sell): adds route + tabs section with sell option

--- src/components/pages/triggers/TriggersRoutes.tsx
+++ src/components/pages/triggers/TriggersRoutes.tsx
@@ -1,17 +1,31 @@
+import {
+  StyledNavTabProps,
+  /* eslint-disable-next-line import/no-unresolved */
+} from '@rsksmart/rif-ui/dist/components/atoms/StyledNavTab'
 import React, { FC, useEffect } from 'react'
 import {
-  Redirect, Route, Switch, useHistory,
+  Redirect, Route, Switch, useHistory, useLocation,
 } from 'react-router-dom'
-
 import networkConfig from 'config'
 import ROUTES from 'routes'
 import Logger from 'utils/Logger'
-import { NotFound } from '..'
+import TabsTemplate from 'components/templates/TabsTemplate'
+import { getTabValueFromLocation } from 'utils/utils'
 import TriggersLandingPage from './TriggersLandingPage'
+import { NotFound } from '..'
 
 const logger = Logger.getInstance()
 
+const TABS: StyledNavTabProps[] = [
+  {
+    label: 'Sell',
+    to: ROUTES.TRIGGERS.SELL.BASE,
+    value: ROUTES.TRIGGERS.SELL.BASE,
+  },
+]
+
 const TriggersRoutes: FC = () => {
+  const { pathname } = useLocation()
   const { services } = networkConfig
   const triggersEnabled = services && services.triggers
   const history = useHistory()
@@ -33,10 +47,34 @@ const TriggersRoutes: FC = () => {
         <Redirect
           exact
           from={ROUTES.TRIGGERS.BASE}
-          to={ROUTES.TRIGGERS.BUY.BASE}
+          to={ROUTES.TRIGGERS.SELL.BASE}
         />
 
-        <Route component={NotFound} />
+        <TabsTemplate
+          title="Notification Services"
+          value={getTabValueFromLocation(
+            TABS, ROUTES.TRIGGERS.SELL.BASE,
+          )(pathname)}
+          tabs={TABS}
+        >
+          <Switch>
+            <Route path={ROUTES.TRIGGERS.SELL.BASE}>
+              <Switch>
+                <Redirect
+                  exact
+                  from={ROUTES.TRIGGERS.SELL.BASE}
+                  to={ROUTES.TRIGGERS.SELL.BASE}
+                />
+                <Route
+                  exact
+                  path={ROUTES.TRIGGERS.SELL.BASE}
+                // component={TriggersSellPage}
+                />
+                <Route component={NotFound} />
+              </Switch>
+            </Route>
+          </Switch>
+        </TabsTemplate>
       </Switch>
     )
   }
