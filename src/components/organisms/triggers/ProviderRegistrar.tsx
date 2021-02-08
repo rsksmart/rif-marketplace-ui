c23d00c48d12f1c2642766b9a13e5c65f09b9477
Alvaro Fariña <farinalvaro@gmail.com>
2021-02-08 15:50:45 -0300

    feat(triggers-sell): handles form data with react-hook-form

--- src/components/organisms/triggers/ProviderRegistrar.tsx
+++ src/components/organisms/triggers/ProviderRegistrar.tsx
@@ -6,76 +6,90 @@ import CenteredContent from 'components/molecules/CenteredContent'
 import RoundBtn from 'components/atoms/RoundBtn'
 import GridItem from 'components/atoms/GridItem'
 import Typography from '@material-ui/core/Typography'
+import { useForm } from 'react-hook-form'
+
+type Inputs = {
+  endpointUrl: string
+  providerAddress: string
+}
 
 type ProviderRegistrarProps = {
-  providerAddress: string
-  onProviderAddrChange: (address: string) => void
-  endpointUrl: string
-  onEndpointChange: (endpoint: string) => void
+  onRegister: (data: Inputs) => void
 }
 
 const ProviderRegistrar: FC<ProviderRegistrarProps> = ({
-  providerAddress,
-  onProviderAddrChange,
-  endpointUrl,
-  onEndpointChange,
-}) => (
-  <GridRow justify="center">
-    <TitledRoundedCard
-      title="General features"
-      titleProps={{ variant: 'h6', color: 'primary', align: 'left' }}
-      roundedCardProps={{ color: 'primary' }}
-    >
-      <CenteredContent>
-        <GridRow spacing={3}>
-          <GridItem xs={12}>
-            <TextField
-              required
-              fullWidth
-              label="Provider address"
-              value={providerAddress}
-              inputProps={{
-                style: { textAlign: 'center' },
-              }}
-              onChange={(
-                { target: { value } },
-              ): void => onProviderAddrChange(value)}
-            />
-          </GridItem>
-          <GridItem xs={12}>
-            <TextField
-              required
-              fullWidth
-              label="Endpoint url"
-              value={endpointUrl}
-              inputProps={{
-                style: { textAlign: 'center' },
-              }}
-              onChange={
-                ({ target: { value } }): void => onEndpointChange(value)
-              }
-            />
-          </GridItem>
-          <GridItem xs={12}>
-            <GridRow justify="center" spacing={2}>
-              <RoundBtn>
-                Register provider
-              </RoundBtn>
-              <Typography
-                color="secondary"
-                variant="subtitle1"
-                align="center"
-                gutterBottom
-              >
-                {`Your wallet will open and you will be asked to confirm
-             the transaction for listing your service.`}
-              </Typography>
+  onRegister,
+}) => {
+  const { register, handleSubmit, errors } = useForm<Inputs>()
+
+  return (
+    <GridRow justify="center">
+      <TitledRoundedCard
+        title="General features"
+        titleProps={{ variant: 'h6', color: 'primary', align: 'left' }}
+        roundedCardProps={{ color: 'primary' }}
+      >
+        <CenteredContent>
+          <form onSubmit={handleSubmit(onRegister)}>
+            <GridRow spacing={3}>
+              <GridItem xs={12}>
+                <TextField
+                  fullWidth
+                  label="Provider address"
+                  inputProps={{
+                    style: { textAlign: 'center' },
+                  }}
+                  inputRef={register({ required: true })}
+                  name="providerAddress"
+                />
+                {
+                  errors.providerAddress && (
+                    <Typography color="error" variant="caption">
+                      Provider address is required
+                    </Typography>
+                  )
+                }
+              </GridItem>
+              <GridItem xs={12}>
+                <TextField
+                  fullWidth
+                  label="Endpoint url"
+                  inputProps={{
+                    style: { textAlign: 'center' },
+                  }}
+                  inputRef={register({ required: true })}
+                  name="endpointUrl"
+                />
+                {
+                  errors.endpointUrl && (
+                    <Typography color="error" variant="caption">
+                      Endpoint URL is required
+                    </Typography>
+                  )
+                }
+              </GridItem>
+              <GridItem xs={12}>
+                <GridRow justify="center" spacing={2}>
+                  <RoundBtn type="submit">
+                    Register provider
+                  </RoundBtn>
+                  <Typography
+                    color="secondary"
+                    variant="subtitle1"
+                    align="center"
+                    gutterBottom
+                  >
+                    {`Your wallet will open and you will be asked to confirm
+                   the transaction for listing your service.`}
+                  </Typography>
+                </GridRow>
+              </GridItem>
             </GridRow>
-          </GridItem>
-        </GridRow>
-      </CenteredContent>
-    </TitledRoundedCard>
-  </GridRow>
-)
+          </form>
+        </CenteredContent>
+      </TitledRoundedCard>
+    </GridRow>
+  )
+}
 
 export default ProviderRegistrar
