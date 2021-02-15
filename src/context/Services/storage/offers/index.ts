3be58df78bb71080dd567cd11d0bd1dad175bfdf
Juraj Piar <juraj.piar@icloud.com>
2021-02-15 16:09:03 +0100

    refactor(storage): standardises offers context

--- src/context/Services/storage/offers/index.ts
+++ src/context/Services/storage/offers/index.ts
@@ -1,15 +1,23 @@
 import withContext, {
   Context,
   Provider,
-  Props,
+  initialState,
 } from './OffersContext'
+import {
+  Props,
+  Actions,
+  State,
+} from './interfaces'
 
 export {
   Context as StorageOffersContext,
   Provider as StorageOffersContextProvider,
+  initialState as storageOffersContextInitialState,
 }
 
 export type StorageOffersContextProps = Props
+export type StorageOffersContextState = State
+export type StorageOffersContextActions = Actions
 
 const withStorageOffersContext = withContext
 
---
+++ src/context/Services/storage/offers/interfaces.ts
@@ -0,0 +1,75 @@
+import { ServiceState } from 'context/Services/interfaces'
+import { StorageOffersFilters } from 'models/marketItems/StorageFilters'
+import { StorageItem } from 'models/marketItems/StorageItem'
+import { Dispatch } from 'react'
+import { Modify } from 'utils/typeUtils'
+import { StorageOrder } from '../interfaces'
+
+// STATE
+export type ContextName = 'storage_offers'
+
+export type OffersListing = {
+  items: StorageItem[]
+}
+
+export type ContextFilters = Pick<StorageOffersFilters, 'price' | 'size' | 'periods' | 'provider'>
+export type ContextLimits = Pick<StorageOffersFilters, 'price' | 'size'>
+
+export type State = Modify<ServiceState<StorageItem>, {
+  listing: OffersListing
+  order?: StorageOrder
+  filters: ContextFilters
+  limits: ContextLimits
+}>
+
+// PAYLOAD
+export type ListingPayload = Pick<OffersListing, 'items'>
+export interface RefreshPayload {
+  refresh: boolean
+}
+
+export type FiltersLimits = Partial<StorageOffersFilters>
+export type LimitsPayload = Pick<StorageOffersFilters, 'price' | 'size'>
+
+export type OrderPayload = Omit<StorageOrder, 'isProcessing'>
+
+// ACTIONS
+export type Action =
+  | {
+    type: 'SET_LISTING'
+    payload: ListingPayload
+  }
+  | {
+    type: 'REFRESH'
+    payload: RefreshPayload
+  }
+  | {
+    type: 'FILTER'
+    payload: Partial<StorageOffersFilters>
+  }
+  | {
+    type: 'UPDATE_LIMITS'
+    payload: LimitsPayload
+  }
+  | {
+    type: 'CLEAN_UP'
+  }
+  | {
+    type: 'SET_ORDER'
+    payload: OrderPayload
+  }
+
+export type Actions = {
+  SET_LISTING: (state: State, payload: ListingPayload) => State
+  REFRESH: (state: State, payload: RefreshPayload) => State
+  FILTER: (state: State, payload: FiltersLimits) => State
+  UPDATE_LIMITS: (state: State, payload: LimitsPayload) => State
+  CLEAN_UP: () => State
+  SET_ORDER: (state: State, payload: OrderPayload) => State
+}
+
+// PROPS
+export type Props = {
+  state: State
+  dispatch: Dispatch<Action>
+}
