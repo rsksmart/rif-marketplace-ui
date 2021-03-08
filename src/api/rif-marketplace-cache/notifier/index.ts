0baa10de90e6b90cc6cff37529701fc661b8d166
Juraj Piar <juraj.piar@icloud.com>
2021-03-08 10:01:34 +0100

    feat(triggers): adds svc 2 appContext

--- src/api/rif-marketplace-cache/notifier/index.ts
+++ src/api/rif-marketplace-cache/notifier/index.ts
@@ -1,3 +1,10 @@
 import { Address } from './interfaces'
+import OffersService, { notifierOffersAddress } from './offers/index'
+
+const notifierAddresses = [notifierOffersAddress]
 
 export type NotifierServiceAddress = Address
+export {
+  OffersService as NotifierOffersService,
+  notifierAddresses,
+}
