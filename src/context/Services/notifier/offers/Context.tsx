e3569eeefc3052eee8bc30bc1703288427e81e4a
Juraj Piar <juraj.piar@icloud.com>
2021-03-09 13:30:19 +0100

    feat(triggers): connects offers context n api

--- src/context/Services/notifier/offers/Context.tsx
+++ src/context/Services/notifier/offers/Context.tsx
@@ -1,9 +1,15 @@
-import React, { createContext, FC, useReducer } from 'react'
+import React, {
+  createContext, FC, useContext, useEffect, useReducer, useState,
+} from 'react'
 import createWithContext from 'context/storeUtils/createWithContext'
 import createReducer from 'context/storeUtils/reducer'
 
-import actions from './actions'
+import AppContext, { AppContextProps } from 'context/App'
+import useErrorReporter from 'hooks/useErrorReporter'
+import { NotifierOffersService } from 'api/rif-marketplace-cache/notifier'
+import { UIError } from 'models/UIMessage'
 import { State, Props } from './interfaces'
+import actions from './actions'
 
 export const contextName = 'notifier_offers' as const
 
@@ -20,6 +26,70 @@ export const Context = createContext<Props>({
 })
 
 export const Provider: FC = ({ children }) => {
+  const [isInitialised, setIsInitialised] = useState(false)
+
+  const {
+    state: appState,
+    dispatch: appDispatch,
+  }: AppContextProps = useContext(AppContext)
+  const api: NotifierOffersService = appState.apis['notifier/v0/offers'] as NotifierOffersService
+
+  const reportError = useErrorReporter()
+
+  // Initialise
+  if (!api.service) {
+    api.connect(reportError)
+  }
+
+  useEffect(() => {
+    if (api?.service && !isInitialised) {
+      try {
+        setIsInitialised(true)
+      } catch (e) {
+        setIsInitialised(false)
+      }
+    }
+  }, [api, isInitialised])
+
+  // Fetch data
+  useEffect(() => {
+    if (isInitialised) {
+      appDispatch({
+        type: 'SET_IS_LOADING',
+        payload: {
+          isLoading: true,
+          id: 'data',
+        },
+      })
+      api.fetch()
+        .then((items) => {
+          dispatch({
+            type: 'SET_LISTING',
+          })
+        })
+        .catch((error) => {
+          reportError(new UIError({
+            error,
+            id: 'service-fetch',
+            text: 'Notifier API Error while fetching data.',
+          }))
+        })
+        .finally(() => {
+          appDispatch({
+            type: 'SET_IS_LOADING',
+            payload: {
+              isLoading: false,
+              id: 'data',
+            },
+          })
+        })
+    }
+  }, [
+    appDispatch,
+    isInitialised,
+    api,
+  ])
+
   const [state, dispatch] = useReducer(
     createReducer(initialState, actions),
     initialState,
