bfb805f6a81c7ae81b9ffac90232065003026a4e
Juraj Piar <juraj.piar@icloud.com>
2021-03-09 15:06:40 +0100

    feat(triggers): updates offers models
    - adds defalut fn apiService errorReporter

--- src/models/marketItems/NotifierItem.ts
+++ src/models/marketItems/NotifierItem.ts
@@ -1,11 +1,20 @@
-import Big from 'big.js'
 import { Item } from 'models/Market'
 import { SupportedTokenSymbol } from 'models/Token'
+import Big from 'big.js'
+
+export type PlanPrice = {
+    token: SupportedTokenSymbol
+    value: Big
+}
+
+export type NotifierPlan = {
+    id: string
+    channels: string[]
+    limit: number
+    priceOptions: PlanPrice[]
+    expirationDate: Date
+}
 
 export type NotifierOfferItem = Item & {
-    provider: string
-    amountRange: number[]
-    channels: string[]
-    currencies: SupportedTokenSymbol[]
-    priceFiatRange: Big[]
+    plans: NotifierPlan[]
 }
