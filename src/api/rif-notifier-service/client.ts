694c4bf7882253b11511531ac1b6ca969f5df7b2
Juraj Piar <juraj.piar@icloud.com>
2021-04-13 19:09:32 +0100

    feat(triggers): connects myOffers to notifier api

--- src/api/rif-notifier-service/client.ts
+++
@@ -1,6 +0,0 @@
-import { createRestClient } from 'api/client'
-import { NotifierAPIService } from './interfaces'
-
-export const NOTIFIER_ADDRESS = process.env.REACT_APP_NOTIFIER_ADDR || 'http://localhost:3333'
-
-export default createRestClient<NotifierAPIService>(NOTIFIER_ADDRESS)
