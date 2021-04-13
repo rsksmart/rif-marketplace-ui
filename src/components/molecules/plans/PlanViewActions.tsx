eeb5f5f4f6d0854750f5c9401ac44b7290500721
Juraj Piar <juraj.piar@icloud.com>
2021-04-13 12:02:07 +0100

    feat(triggers): adds offer/plan view components

---
+++ src/components/molecules/plans/PlanViewActions.tsx
@@ -0,0 +1,53 @@
+import Grid, { GridProps } from '@material-ui/core/Grid'
+import { Button } from '@rsksmart/rif-ui'
+import { ButtonProps } from '@material-ui/core/Button' // FIXME: this is meant to come from RUI, but @rsksmart/rif-ui/dist/components/atoms/Button module cannot be found. FIX RUI!
+// import Button, { ButtonProps } from '@rsksmart/rif-ui/dist/components/atoms/Button'
+import React, {
+  FC,
+} from 'react'
+
+type Props = GridProps & {
+    editProps: ButtonProps
+    cancelProps: ButtonProps
+}
+
+const PlanViewActions: FC<Props> = ({ editProps, cancelProps, ...props }) => (
+  <Grid
+    container
+    justify="flex-end"
+    spacing={1}
+    {...props}
+  >
+    <Grid item>
+      <Button
+        variant="outlined"
+        color="primary"
+        rounded
+        {...editProps}
+        // onClick={onEditOffer}
+        // disabled={!storageOffer.isActive}
+      >
+        Edit
+      </Button>
+    </Grid>
+    <Grid item>
+      <Button
+        variant="outlined"
+        rounded
+        color="primary"
+        {...cancelProps}
+        // onClick={handleCancelOpen}
+        // disabled={!storageOffer.isActive}
+      >
+        Cancel
+      </Button>
+    </Grid>
+    {/* <CancelOfferDialogue
+        open={cancelOfferOpen}
+        onClose={handleCancelClose}
+        onConfirmCancel={handleCancelation}
+    /> */}
+  </Grid>
+)
+
+export default PlanViewActions
