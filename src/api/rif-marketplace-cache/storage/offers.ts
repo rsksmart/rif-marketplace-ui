47b88d56d9f80763a0c023e17a9121480c527505
Juraj Piar <juraj.piar@icloud.com>
2021-04-15 11:39:47 +0100

    fix(storage): fixes price,size filters

--- src/api/rif-marketplace-cache/storage/offers.ts
+++ src/api/rif-marketplace-cache/storage/offers.ts
@@ -2,13 +2,11 @@ import { Paginated } from '@feathersjs/feathers'
 import { AbstractAPIService, isResultPaginated } from 'api/models/apiService'
 import OfferFiltersTransport from 'api/models/storage/OfferFiltersTransport'
 import { OfferTransport } from 'api/models/storage/transports'
-import { MinMaxFilter } from 'models/Filters'
 import { StorageOffersFilters } from 'models/marketItems/StorageFilters'
 import { StorageItem, StorageOffer } from 'models/marketItems/StorageItem'
-import { UNIT_PREFIX_POW2 } from 'utils/utils'
 import client from '../client'
 import { StorageAPIService, StorageWSChannel } from './interfaces'
-import { mapOfferFromTransport, MinMax } from './utils'
+import { mapOfferFromTransport } from './utils'
 
 export const offersAddress = 'storage/v0/offers' as const
 export type OffersAddress = typeof offersAddress
@@ -40,46 +38,5 @@ export class StorageOffersService extends AbstractAPIService
     this.meta = metadata
 
     return data.map(this.mapFromTransport)
-  }
-
-  fetchSizeLimits = async (): Promise<MinMaxFilter> => {
-    const minMB = await this.fetchMinMaxLimit(
-      this.service,
-      MinMax.min,
-      'totalCapacity',
-    )
-    const maxMB = await this.fetchMinMaxLimit(
-      this.service,
-      MinMax.max,
-      'totalCapacity',
-    )
-    return {
-      min: minMB / UNIT_PREFIX_POW2.KILO,
-      max: maxMB / UNIT_PREFIX_POW2.KILO,
-    }
-  }
-
-  private fetchMinMaxLimit = async (
-    service,
-    minMax: MinMax,
-    filedName: string,
-    options?: {
-      selectField?: string
-    },
-  ): Promise<number> => {
-    const select = options?.selectField || filedName
-    const query = {
-      $limit: 1,
-      $sort: {
-        [filedName]: minMax,
-      },
-      $select: [select],
-    }
-    const result = await service.find({ query })
-
-    return result.reduce((_, item): number => {
-      const round = minMax === MinMax.min ? Math.floor : Math.ceil
-      return round(parseFloat(item[select]))
-    }, 0)
   }
 }
