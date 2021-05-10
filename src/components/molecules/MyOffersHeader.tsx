793b9b431a9d11504171383c4ea8f75ffa031442
Juraj Piar <juraj.piar@icloud.com>
2021-05-10 19:23:55 +0100

    feat(notifier): adds and refactors tab header

--- src/components/molecules/MyOffersHeader.tsx
+++ src/components/molecules/MyOffersHeader.tsx
@@ -1,29 +1,16 @@
-import {
-  Grid, Typography,
-} from '@material-ui/core'
 import handProvidingFunds from 'assets/images/handProvidingFunds.svg'
 import React, {
   FC,
 } from 'react'
+import TabHeader from './TabHeader'
 
-type Props = { title?: string }
-
-const MyOffersHeader: FC<Props> = ({ title, children }) => (
-  <Grid container alignItems="center" justify="space-between">
-    <Grid container item xs={10} alignItems="center">
-      <Grid item xs={1}>
-        <img src={handProvidingFunds} alt="hand providing funds" />
-      </Grid>
-      <Grid item xs={11} md="auto">
-        <Typography gutterBottom variant="h6" color="primary">
-          {title ?? 'You are providing the following plans to your customers'}
-        </Typography>
-      </Grid>
-    </Grid>
-    <Grid container item xs={2} justify="flex-end">
-      {children}
-    </Grid>
-  </Grid>
+const MyOffersHeader: FC = ({ children }) => (
+  <TabHeader
+    title="You are providing the following plans to your customers"
+    icon={<img src={handProvidingFunds} alt="hand providing funds" />}
+  >
+    {children}
+  </TabHeader>
 )
 
 export default MyOffersHeader
