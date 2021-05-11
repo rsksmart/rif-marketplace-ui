3b66708bec0290b462adbd16e640d03dbe2835a4
rafaiovlabs <76968120+rafaiovlabs@users.noreply.github.com>
2021-05-11 11:49:31 -0500

    feat(notifier): adds notification channels

--- src/components/organisms/notifier/NotificationChannelCreate.tsx
+++ src/components/organisms/notifier/NotificationChannelCreate.tsx
@@ -1,12 +1,15 @@
 import React, { FC } from 'react'
 import { makeStyles, Theme } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
-import { NotifierChannel, NotifierChannelType } from 'models/marketItems/NotifierItem'
+import { NotifierChannel, notifierChannelType } from 'models/marketItems/NotifierItem'
 import { FormControl } from '@material-ui/core'
 import Select from '@material-ui/core/Select'
 import MenuItem from '@material-ui/core/MenuItem'
 import TextField from '@material-ui/core/TextField'
 import { SelectRowButton } from 'components/molecules'
+import { useForm } from 'react-hook-form'
+import Typography from '@material-ui/core/Typography'
+import validateURL from 'utils/validationUtils'
 
 export interface NotificationChannelCreateProps {
   onChannelChange: (e: any) => void
@@ -27,11 +30,21 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
 }))
 
+type Inputs = {
+    destination: string
+}
+
 const NotificationChannelCreate: FC<NotificationChannelCreateProps> = ({
   notifierChannel, availableChannels, onChannelChange, onDestinationChange, channelAdd,
 }) => {
+  const validateDestination = (destination: string) => {
+    if (notifierChannel.type === 'API') {
+      return validateURL(destination)
+    }
+    return true
+  }
   const classes = useStyles()
-
+  const { register, handleSubmit, errors } = useForm<Inputs>()
   return (
     <Grid container spacing={4} alignItems="center">
       <Grid item xs={2} md={2}>
@@ -50,17 +63,26 @@ const NotificationChannelCreate: FC<NotificationChannelCreateProps> = ({
       </Grid>
       <Grid item xs={7} md={7}>
         <TextField
+          name="destination"
           fullWidth
           onChange={onDestinationChange}
-          placeholder={NotifierChannelType[notifierChannel.type]}
+          placeholder={notifierChannelType[notifierChannel.type]}
           variant="outlined"
+          inputRef={register({ required: true, validate: validateDestination })}
           InputProps={{
             style: { height: 40 },
           }}
         />
+        {
+              errors.destination && (
+              <Typography color="error" variant="caption">
+                Invalid destination
+              </Typography>
+              )
+          }
       </Grid>
       <Grid item xs={3} md={3}>
-        <SelectRowButton id="add-channel" handleSelect={channelAdd} size="large" variant="outlined">Add Channel</SelectRowButton>
+        <SelectRowButton id="add-channel" handleSelect={handleSubmit(channelAdd)} size="large" variant="outlined">Add Channel</SelectRowButton>
       </Grid>
     </Grid>
 
--- src/utils/validationUtils.ts
+++ src/utils/validationUtils.ts
@@ -1,4 +1,4 @@
-export const validateURL = (_url: string): boolean => {
+const validateURL = (_url: string): boolean => {
   let url
   try {
     url = new URL(_url)
@@ -8,11 +8,4 @@ export const validateURL = (_url: string): boolean => {
   return url.protocol === 'http:' || url.protocol === 'https:'
 }
 
-export const validateEmail = (emails: string): boolean => {
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  let valid = true
-  emails.split(';').forEach((email) => {
-    if (re.test(String(email).toLowerCase()) === false) valid = false
-  })
-  return valid
-}
+export default validateURL
