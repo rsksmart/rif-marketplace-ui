72915a51472c05a04321e12b076bf49635b116bd
Alvaro Fariña <farinalvaro@gmail.com>
2021-05-20 17:26:55 -0300

    feat(notifier): adds support for different quotations per currency

--- src/components/organisms/notifier/buy/CheckoutStepper.tsx
+++ src/components/organisms/notifier/buy/CheckoutStepper.tsx
@@ -1,10 +1,13 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useContext, useState } from 'react'
 import Stepper from '@material-ui/core/Stepper'
 import Step from '@material-ui/core/Step'
 import StepLabel from '@material-ui/core/StepLabel'
 import StepContent from '@material-ui/core/StepContent'
 import { Button } from '@rsksmart/rif-ui'
 import RoundBtn from 'components/atoms/RoundBtn'
+import { SYSTEM_SUPPORTED_FIAT } from 'models/Fiat'
+import { QuotationPerToken } from 'models/Market'
+import MarketContext from 'context/Market'
 import CheckoutPayment from './CheckoutPayment'
 
 type Props = {
@@ -13,8 +16,21 @@ type Props = {
 
 const CheckoutStepper: FC<Props> = ({ onBuy }) => {
   const [activeStep, setActiveStep] = useState(0)
+  const {
+    state: {
+      exchangeRates: {
+        crypto: cryptoXRs,
+      },
+    },
+  } = useContext(MarketContext)
+
   const handleNext = (): void => setActiveStep(1)
   const handleBack = (): void => setActiveStep(0)
+  // TODO: remove hardcoded
+  const paymentOptions: QuotationPerToken = {
+    rif: '100',
+    rbtc: '0.001',
+  }
 
   return (
     <Stepper activeStep={activeStep} orientation="vertical">
@@ -34,8 +50,13 @@ const CheckoutStepper: FC<Props> = ({ onBuy }) => {
       <Step>
         <StepLabel>Payment</StepLabel>
         <StepContent>
-          <CheckoutPayment onBuy={onBuy} />
-          {/* <div> */}
+          <CheckoutPayment
+            onBuy={onBuy}
+            paymentOptions={paymentOptions}
+            fiatUnit={SYSTEM_SUPPORTED_FIAT.usd}
+            expirationDate="01/01/2022"
+            cryptoXRs={cryptoXRs}
+          />
           <Button
             onClick={handleBack}
             variant="outlined"
@@ -43,8 +64,7 @@ const CheckoutStepper: FC<Props> = ({ onBuy }) => {
             rounded
           >
             Back
-            </Button>
-          {/* </div> */}
+          </Button>
         </StepContent>
       </Step>
     </Stepper>
