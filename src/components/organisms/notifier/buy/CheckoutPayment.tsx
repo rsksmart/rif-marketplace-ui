30125ac693ce3ab3860fde4806f0bca8e3a5e41b
Alvaro Fariña <farinalvaro@gmail.com>
2021-05-21 18:42:48 -0300

    feat(notifier): PriceSummary component styles and better typings - CheckoutPayment component styles

--- src/components/organisms/notifier/buy/CheckoutPayment.tsx
+++ src/components/organisms/notifier/buy/CheckoutPayment.tsx
@@ -1,38 +1,42 @@
 import RoundBtn from 'components/atoms/RoundBtn'
-import React, { FC, useEffect, useState } from 'react'
+import React, { FC, useState } from 'react'
 import Typography from '@material-ui/core/Typography'
 import GridRow from 'components/atoms/GridRow'
-import GridItem from 'components/atoms/GridItem'
-import Grid from '@material-ui/core/Grid'
+import Grid, { GridProps } from '@material-ui/core/Grid'
 import { SupportedTokenSymbol } from 'models/Token'
 import CurrencySelect from 'components/molecules/CurrencySelect'
-import { SUPPORTED_FIAT, SYSTEM_SUPPORTED_FIAT } from 'models/Fiat'
+import { SYSTEM_SUPPORTED_FIAT } from 'models/Fiat'
 import { MarketCryptoRecord, QuotationPerToken } from 'models/Market'
-import Big from 'big.js'
+import ExpirationDate from 'components/molecules/ExpirationDate'
+import PriceSummary from 'components/molecules/PriceSummary'
+import { Box, makeStyles, Theme } from '@material-ui/core'
+import RoundedCard from 'components/atoms/RoundedCard'
 
 type Props = {
   onBuy: () => void
   paymentOptions: QuotationPerToken
-  // to fully support multifiat we need a refactor
-  // cryptoXR should be able to provide rate per crypto per fiat
   fiatUnit: SYSTEM_SUPPORTED_FIAT
-  expirationDate: string
+  expirationDate: Date
   cryptoXRs: MarketCryptoRecord
 }
+
+const useStyles = makeStyles((theme: Theme) => ({
+  priceSummaryCard: {
+    padding: theme.spacing(1.5, 3),
+    marginBottom: theme.spacing(1),
+  },
+  expirationDate: {
+    justifyContent: 'center',
+  },
+}))
 
 const CheckoutPayment: FC<Props> = ({
   onBuy, paymentOptions, fiatUnit, expirationDate, cryptoXRs,
 }) => {
-  const currencyOptions = Object.keys(paymentOptions) as SupportedTokenSymbol[]
-  const [fiatFinalPrice, setFiatFinalPrice] = useState('')
-  const [selectedCurrency, setSelectedCurrency] = useState(currencyOptions[0])
+  const classes = useStyles()
 
-  useEffect(() => {
-    const { rate } = cryptoXRs[selectedCurrency]
-    const tokenPrice = paymentOptions[selectedCurrency]
-    const fiatPrice = Big(tokenPrice).mul(rate).toFixed(2)
-    setFiatFinalPrice(fiatPrice)
-  }, [selectedCurrency, cryptoXRs, paymentOptions])
+  const currencyOptions = Object.keys(paymentOptions) as SupportedTokenSymbol[]
+  const [selectedCurrency, setSelectedCurrency] = useState(currencyOptions[0])
 
   const handleCurrencyChange = (
     { target: { value } }: React.ChangeEvent<{ name?: string, value: unknown }>,
@@ -40,38 +44,61 @@ const CheckoutPayment: FC<Props> = ({
     setSelectedCurrency(value as SupportedTokenSymbol)
   }
 
+  const colProps: GridProps = {
+    container: true,
+    item: true,
+    direction: 'column',
+    md: 4,
+    sm: 12,
+    alignItems: 'center',
+    justify: 'center',
+  }
+
   return (
     <>
-      <GridRow>
-        <GridItem md={4} sm={12}>
-          <p>Select currency</p>
-          <GridItem xs={12}>
-            <CurrencySelect
-              options={currencyOptions}
-              value={selectedCurrency}
-              onChange={handleCurrencyChange}
+      <Typography component="div" variant="caption" gutterBottom>
+        <Box>
+          {`To acquire this notification service you have to select 
+          the currency to get the final price.`}
+        </Box>
+        <Box>
+          You can add more events to this contract before or after renew.
+        </Box>
+      </Typography>
+      <GridRow spacing={3} alignItems="center">
+        <Grid {...colProps}>
+          <Typography>Select currency</Typography>
+          <CurrencySelect
+            options={currencyOptions}
+            value={selectedCurrency}
+            onChange={handleCurrencyChange}
+          />
+        </Grid>
+        <Grid {...colProps}>
+          <RoundedCard className={classes.priceSummaryCard} color="primary">
+            <PriceSummary
+              cryptoPrice={paymentOptions[selectedCurrency]}
+              cryptoXRs={cryptoXRs}
+              currency={selectedCurrency}
+              fiatUnit={fiatUnit}
             />
-          </GridItem>
-        </GridItem>
-        <GridItem md={4} sm={12}>
-          <GridItem xs={12}>
-            <Typography>Total price</Typography>
-            <Typography color="primary">
-              {`${paymentOptions[selectedCurrency]} ${selectedCurrency}`}
-            </Typography>
-            <Typography>
-              {`Fiat price ${fiatFinalPrice} ${SUPPORTED_FIAT[fiatUnit].displayName}`}
-            </Typography>
-          </GridItem>
-          <GridItem xs={12}>
-            {`Expiration date: ${expirationDate}`}
-          </GridItem>
-        </GridItem>
-        <Grid container item md={4} sm={12} justify="center">
-          <RoundBtn onClick={onBuy}>Buy</RoundBtn>
+          </RoundedCard>
+          <GridRow justify="center">
+            <Typography color="secondary" align="center">Expiration date</Typography>
+            <ExpirationDate
+              className={classes.expirationDate}
+              type="normal"
+              date={expirationDate}
+            />
+          </GridRow>
+        </Grid>
+        <Grid {...colProps}>
+          <GridRow justify="center">
+            <RoundBtn onClick={onBuy}>Buy</RoundBtn>
+          </GridRow>
           <Typography
             color="secondary"
-            variant="subtitle1"
+            variant="caption"
             align="center"
           >
             {`Your wallet will open and you will be asked
