1d0541509e16c62e93cdb74a80b43e05dd080ae7
Juraj Piar <juraj.piar@icloud.com>
2021-05-27 08:10:54 +0100

    refactor(notifier): makes use of getFiatPrice

--- src/components/molecules/PriceSummary.tsx
+++ src/components/molecules/PriceSummary.tsx
@@ -4,6 +4,7 @@ import Big from 'big.js'
 import GridItem from 'components/atoms/GridItem'
 import { TokenXR } from 'models/Market'
 import React, { FC } from 'react'
+import { getFiatPrice } from 'utils/tokenUtils'
 
 export type Props = {
   className?: string
@@ -14,28 +15,24 @@ export type Props = {
 }
 
 const PriceSummary: FC<Props> = ({
-  className = '', title = 'Total Price', cryptoPrice, tokenXR, fiatDisplayName,
-}) => {
-  const { rate, displayName: tokenDisplayName } = tokenXR
-  const fiatPrice = Big(cryptoPrice).mul(rate).toFixed(2)
-
-  return (
-    <GridItem className={className} xs={12}>
-      <Typography component="div" align="center">
-        <Box>{title}</Box>
-        <Box
-          fontWeight="fontWeightMedium"
-          fontSize="h6.fontSize"
-          color="primary.main"
-        >
-          {`${cryptoPrice} ${tokenDisplayName}`}
-        </Box>
-        <Box color="text.secondary">
-          {`${fiatPrice} ${fiatDisplayName}`}
-        </Box>
-      </Typography>
-    </GridItem>
-  )
-}
+  className = '', title = 'Total Price',
+  cryptoPrice, tokenXR, fiatDisplayName,
+}) => (
+  <GridItem className={className} xs={12}>
+    <Typography component="div" align="center">
+      <Box>{title}</Box>
+      <Box
+        fontWeight="fontWeightMedium"
+        fontSize="h6.fontSize"
+        color="primary.main"
+      >
+        {`${cryptoPrice} ${tokenXR.displayName}`}
+      </Box>
+      <Box color="text.secondary">
+        {`${getFiatPrice(cryptoPrice, tokenXR)} ${fiatDisplayName}`}
+      </Box>
+    </Typography>
+  </GridItem>
+)
 
 export default PriceSummary
