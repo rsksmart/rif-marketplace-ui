1111aba01eff8615c2d1215d1567d47b44ec66f0
Juraj Piar <juraj.piar@icloud.com>
2021-05-28 11:39:10 +0100

    feat(notifier): shows events in view own purchases
    refactor

--- src/api/rif-marketplace-cache/notifier/subscriptions/models.ts
+++ src/api/rif-marketplace-cache/notifier/subscriptions/models.ts
@@ -1,16 +1,43 @@
+import { SupportedEventChannel } from 'config/notifier'
 import { PlanDTO } from '../offers/models'
 
-export type SubscriptionTopic = {
-  notificationPreferences: string | Array<string>
-  type: string
-  topicParams: Array<any>
+export const EVENT_TYPES = {
+  NEW_BLOCK: 'NEW_BLOCK',
+  NEW_TRANSACTIONS: 'NEW_TRANSACTIONS',
+  PENDING_TRANSACTIONS: 'PENDING_TRANSACTIONS',
+  CONTRACT_EVENT: 'CONTRACT_EVENT',
+} as const
+
+export type SubscriptionEventType = keyof typeof EVENT_TYPES
+
+export const EVENT_PARAM_TYPES = {
+  EVENT_PARAM: 'EVENT_PARAM',
+  EVENT_NAME: 'EVENT_NAME',
+  CONTRACT_ADDRESS: 'CONTRACT_ADDRESS',
+} as const
+
+export type TopicParamType = keyof typeof EVENT_PARAM_TYPES
+
+export type EventParam = {
+  type: TopicParamType
+  value: string
+  valueType: string
 }
 
-export type SubscriptionStatus =
-  | 'ACTIVE'
-  | 'PENDING'
-  | 'EXPIRED'
-  | 'COMPLETED'
+export type SubscriptionEvent = {
+  notificationPreferences: SupportedEventChannel | Array<SupportedEventChannel>
+  type: SubscriptionEventType
+  topicParams: Array<EventParam>
+}
+
+export const SUBSCRIPTION_STATUS = {
+  ACTIVE: 'ACTIVE',
+  PENDING: 'PENDING',
+  EXPIRED: 'EXPIRED',
+  COMPLETED: 'COMPLETED',
+} as const
+
+export type SubscriptionStatus = keyof typeof SUBSCRIPTION_STATUS
 
 export type SubscriptionDTO = {
   hash: string
@@ -21,7 +48,7 @@ export type SubscriptionDTO = {
   previousSubscription: string
   expirationDate: Date
   consumer: string
-  topics: Array<SubscriptionTopic>
+  topics: Array<SubscriptionEvent>
   paid: boolean
   price: string
   rateId: string
