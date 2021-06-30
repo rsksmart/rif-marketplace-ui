370c2117000fff620530cf4a9b561af44e36e6c0
Alvaro Fariña <farinalvaro@gmail.com>
2021-06-30 15:12:54 -0300

    feat(notifier): enables renew subscription only when completed or expired

--- src/components/pages/notifier/mypurchase/mapMyPurchases.tsx
+++ src/components/pages/notifier/mypurchase/mapMyPurchases.tsx
@@ -8,6 +8,7 @@ import { ExchangeRate } from 'context/Market/interfaces'
 import MarketplaceAddressCell from 'components/molecules/MarketplaceAddressCell'
 import ExpirationDate, { SubscriptionExpirationType } from 'components/molecules/ExpirationDate'
 import { PlanDTO, PLAN_STATUS } from 'api/rif-marketplace-cache/notifier/offers/models'
+import { SUBSCRIPTION_STATUSES } from 'api/rif-notifier-service/models/subscriptions'
 
 const EXPIRATION_WARNING_TRIGGER = 5
 
@@ -34,6 +35,7 @@ const mapMyPurchases = <V extends Function, R extends Function>(
     expirationDate,
     price,
     provider,
+    status,
   }: NotifierSubscriptionItem):
     MySubscription => {
     const { rate, displayName } = crypto?.[token.symbol]
@@ -41,6 +43,10 @@ const mapMyPurchases = <V extends Function, R extends Function>(
 
     const expType = getExpirationType(plan)
     const { provider: providerAddress } = provider
+    const canRenew = (
+      status === SUBSCRIPTION_STATUSES.COMPLETED
+      || status === SUBSCRIPTION_STATUSES.EXPIRED
+    )
 
     return {
       id,
@@ -70,7 +76,7 @@ const mapMyPurchases = <V extends Function, R extends Function>(
         <MarketplaceActionsCell
           actions={[
             {
-              disabled: expType === 'blocked',
+              disabled: !canRenew,
               id: `renew_${id}`,
               handleSelect: (): void => onRenew(id),
               children: 'Renew',
