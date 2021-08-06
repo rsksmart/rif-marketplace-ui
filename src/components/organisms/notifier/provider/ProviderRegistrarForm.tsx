7829365cd1b674e7940b941669e22057c09e0185
Vijay K <11134695+vjgene@users.noreply.github.com>
2021-08-06 14:06:22 +0200

    fix(notifier): verifies provider registered based on host name (#941)
    * fix(notifier): verifies provider registered based on host name
    
    * fix(notifier): add environment variable for insecure connections

--- src/components/organisms/notifier/provider/ProviderRegistrarForm.tsx
+++ src/components/organisms/notifier/provider/ProviderRegistrarForm.tsx
@@ -7,15 +7,15 @@ import RoundBtn from 'components/atoms/RoundBtn'
 import GridItem from 'components/atoms/GridItem'
 import Typography from '@material-ui/core/Typography'
 import { useForm } from 'react-hook-form'
-import { toChecksum, trailingSlashRegex } from 'utils/stringUtils'
+import { ipRegex, toChecksum } from 'utils/stringUtils'
 import useErrorReporter from 'hooks/useErrorReporter'
 import SubscriptionPlans from 'api/rif-notifier-service/subscriptionPlans'
 import ProvidersService, { notifierProvidersAddress } from 'api/rif-marketplace-cache/notifier/providers'
 import AppContext, { AppContextProps } from 'context/App'
 import {
-  HTTPS_REQUIRED, NO_AVAILABLE_SUBSCRIPTION_PLAN, URL_ALREADY_REGISTERED, WRONG_URL,
+  HTTPS_REQUIRED, IP_NOT_ALLOWED, NO_AVAILABLE_SUBSCRIPTION_PLAN, URL_ALREADY_REGISTERED, WRONG_URL,
 } from 'constants/notifier/strings'
-import { SUPPORTED_PROVIDER_PROTOCOLS } from 'config/notifier'
+import { ALLOW_INSECURE_CONNECTIONS, SUPPORTED_PROVIDER_PROTOCOLS } from 'config/notifier'
 
 type Inputs = {
   endpointUrl: string
@@ -44,23 +44,30 @@ const ProviderRegistrarForm: FC<ProviderRegistrarFormProps> = ({
   } = useContext<AppContextProps>(AppContext)
 
   const validateProviderURL = async (url: string): Promise<boolean | string> => {
+    let providerURL
     try {
-      const { protocol } = new URL(url)
-
-      if (!SUPPORTED_PROVIDER_PROTOCOLS.includes(protocol)) {
-        return HTTPS_REQUIRED
-      }
+      providerURL = new URL(url)
     } catch {
       return WRONG_URL
     }
+    const { protocol, hostname } = providerURL
+
+    if (!ALLOW_INSECURE_CONNECTIONS && (ipRegex.test(hostname.replace(/\[/g, '').replace(/\]/g, '')))) {
+      return IP_NOT_ALLOWED
+    }
+
+    if (!ALLOW_INSECURE_CONNECTIONS && !SUPPORTED_PROVIDER_PROTOCOLS.includes(protocol)) {
+      return HTTPS_REQUIRED
+    }
+
+    providersApi.connect(reportError)
+    const providerService: ProvidersService = providersApi as ProvidersService
+    const urlIsRegistered: boolean = await providerService.isRegisteredURL(providerURL.hostname)
 
     const notifierService: SubscriptionPlans = new SubscriptionPlans(url)
     notifierService.connect(reportError)
-    providersApi.connect(reportError)
-    const providerService: ProvidersService = providersApi as ProvidersService
-    const urlIsRegistered: boolean = await providerService.isRegisteredURL(url.replace(trailingSlashRegex, ''))
     const hasActivePlans = Boolean((await notifierService.getActivePlans()).length)
-    const urlMessage = urlIsRegistered ? URL_ALREADY_REGISTERED : ''
+    const urlMessage = !urlIsRegistered ? '' : URL_ALREADY_REGISTERED
     const planMessage = hasActivePlans ? '' : NO_AVAILABLE_SUBSCRIPTION_PLAN
 
     return (!urlIsRegistered && hasActivePlans) || urlMessage || planMessage
