05cbea34d82f690ad82a9360a9eed5aec6207545
Juraj Piar <juraj.piar@icloud.com>
2021-08-24 12:27:39 +0100

    feat(notifier): makes plan loading async (buy)

--- src/components/pages/notifier/buy/NotifierOffersPage.tsx
+++ src/components/pages/notifier/buy/NotifierOffersPage.tsx
@@ -1,9 +1,11 @@
 import Tooltip from '@material-ui/core/Tooltip'
 import Typography from '@material-ui/core/Typography'
-import { Web3Store } from '@rsksmart/rif-ui'
+import { Spinner, Web3Store } from '@rsksmart/rif-ui'
+import { SubscriptionPlanDTO } from 'api/rif-notifier-service/models/subscriptionPlan'
 import SubscriptionPlans from 'api/rif-notifier-service/subscriptionPlans'
 import ItemWUnit from 'components/atoms/ItemWUnit'
 import { AddressItem, SelectRowButton } from 'components/molecules'
+import { SelectRowButtonProps } from 'components/molecules/SelectRowButton'
 import NotifierOffersFilters from 'components/organisms/filters/notifier/OffersFilters'
 import NotifierPlansDraw from 'components/organisms/notifier/NotifierPlansDraw'
 import MarketPageTemplate from 'components/templates/MarketPageTemplate'
@@ -13,7 +15,7 @@ import { NotifierOffersContext, NotifierOffersContextProps } from 'context/Servi
 import { MarketCryptoRecord } from 'models/Market'
 import { NotifierOfferItem, NotifierPlan, PriceOption } from 'models/marketItems/NotifierItem'
 import React, {
-  FC, useContext, useEffect, useMemo, useState,
+  FC, useCallback, useContext, useEffect, useMemo, useState,
 } from 'react'
 import { useHistory } from 'react-router-dom'
 import ROUTES from 'routes'
@@ -55,12 +57,36 @@ const showPlans = (
   )
 }
 
+const getProviderPlans = (url: string): Promise<SubscriptionPlanDTO[]> => {
+  const notifierService = new SubscriptionPlans(url)
+  notifierService.connect((er) => {
+    Logger.getInstance()
+      .debug(JSON.stringify(er, null, 2))
+  })
+  return notifierService.getActivePlans()
+}
+
+const createActionButton = (actionButtonProps: SelectRowButtonProps): JSX.Element => {
+  const selectButton = (
+    <SelectRowButton {...actionButtonProps} />
+  )
+
+  return actionButtonProps.disabled
+    ? (
+      <Tooltip title="This provider doesn't have any active plans that match our records at the moment">
+        <span>
+          {selectButton}
+        </span>
+      </Tooltip>
+    ) : selectButton
+}
+
 const NotifierOffersPage: FC = () => {
   const history = useHistory()
   const {
     state: {
       contextID,
-      listing: { items },
+      listing: { items: cachedPlans },
     },
     dispatch,
   } = useContext<NotifierOffersContextProps>(NotifierOffersContext)
@@ -98,106 +124,115 @@ const NotifierOffersPage: FC = () => {
     history.push(ROUTES.NOTIFIER.BUY.CHECKOUT)
   }
 
-  const providers = useMemo(() => Array.from(new Set(items
-    .map(({ provider }) => provider))), [items])
+  const providers = useMemo(() => Array.from(new Set(cachedPlans
+    .map(({ provider }) => provider))), [cachedPlans])
   const [selectedProvider, setSelectedProvider] = useState<string>()
   const [
     selectedProviderPlans,
     setSelectedProviderPlans,
   ] = useState<NotifierPlan[]>([])
   const [collection, setCollection] = useState<MarketplaceItem[]>([])
+  const addToCollection = useCallback((newCollectionItem: MarketplaceItem) => {
+    setCollection((curCollection) => {
+      const spinnerlessCollection = curCollection.slice(0, -1)
+      const [spinner] = curCollection.slice(-1)
+
+      return [
+        ...spinnerlessCollection,
+        newCollectionItem,
+        spinner,
+      ]
+    })
+  }, [setCollection])
 
   useEffect(() => {
-    setSelectedProviderPlans(items
+    setSelectedProviderPlans(cachedPlans
       .filter(({ provider }) => provider === selectedProvider))
-  }, [items, selectedProvider])
+  }, [cachedPlans, selectedProvider])
 
   useEffect(() => {
-    if (items?.length) {
-      Promise.all(providers
-        .map<Promise<MarketplaceItem>>(async (provider) => {
-          const providerPlans = items
-            .filter((item) => item.provider === provider)
-          const { url } = providerPlans[0]
+    if (cachedPlans?.length) {
+      setCollection([
+        {
+          id: 'pending',
+          action1: (<Spinner />),
+        },
+      ])
 
-          const notifierService = new SubscriptionPlans(url)
-          notifierService.connect((er) => {
-            Logger.getInstance()
-              .debug(JSON.stringify(er, null, 2))
-          })
-          const notifierActivePlans = await notifierService.getActivePlans()
+      Promise.all(providers.map(async (provider) => {
+        const providerPlans = cachedPlans
+          .filter((item) => item.provider === provider)
+        const [{ url }] = providerPlans
 
-          // filter out inactive plans
-          const activePlans = providerPlans.filter(
-            ({ planId }) => notifierActivePlans.some(
-              ({ id: notifierPlanId }) => notifierPlanId === planId,
-            ),
-          )
-          const hasActivePlans = activePlans.length
+        const notifierActivePlans = await getProviderPlans(url)
 
-          const isSelected = selectedProvider === provider
+        // filter out inactive plans
+        const activePlans = providerPlans.filter(
+          ({ planId }) => notifierActivePlans && notifierActivePlans.some(
+            ({ id: notifierPlanId }) => notifierPlanId === planId,
+          ),
+        )
+        const hasActivePlans = activePlans.length
 
-          const selectButton = (
-            <SelectRowButton
-              disabled={!hasActivePlans}
-              id={provider}
-              isSelected={isSelected}
-              handleSelect={(): void => {
-                setSelectedProvider(isSelected
-                  ? undefined
-                  : provider)
-              }}
+        const isSelected = selectedProvider === provider
+
+        const buttonSelect = createActionButton({
+          disabled: !hasActivePlans,
+          id: provider,
+          isSelected,
+          handleSelect: () => setSelectedProvider(
+            isSelected ? undefined : provider,
+          ),
+        })
+
+        const commonProperties = {
+          id: provider,
+          provider: <AddressItem value={provider} />,
+          action1: account === provider ? 'Your offer' : (buttonSelect),
+        }
+
+        if (!hasActivePlans) {
+          const collectionItem: MarketplaceItem = {
+            ...commonProperties,
+            channels: 'N/A',
+            currencies: 'N/A',
+            notifLimitRange: 'N/A',
+            priceFiatRange: <Typography>N/A</Typography>,
+          }
+          addToCollection(collectionItem)
+          return
+        }
+
+        const {
+          priceFiatRange,
+          ...offerDetails
+        } = mapPlansToOffers(
+          activePlans, crypto,
+        )
+
+        const resultItem: MarketplaceItem = {
+          ...commonProperties,
+          ...offerDetails,
+          priceFiatRange: (
+            <ItemWUnit
+              type="mediumPrimary"
+              value={priceFiatRange}
+              unit={currentFiat}
             />
-          )
-          const action1 = hasActivePlans
-            ? selectButton
-            : (
-              <Tooltip title="This provider doesn't have any active plan at the moment">
-                <span>
-                  {selectButton}
-                </span>
-              </Tooltip>
-            )
+          ),
+        }
 
-          const commonResult = {
-            id: provider,
-            provider: <AddressItem value={provider} />,
-            action1: account === provider ? 'Your offer' : (action1),
-          }
-
-          if (!hasActivePlans) {
-            return {
-              ...commonResult,
-              channels: 'N/A',
-              currencies: 'N/A',
-              notifLimitRange: 'N/A',
-              priceFiatRange: <Typography>N/A</Typography>,
-            }
-          }
-
-          const {
-            priceFiatRange,
-            ...offerDetails
-          } = mapPlansToOffers(
-            activePlans, crypto,
-          )
-
-          return {
-            ...commonResult,
-            ...offerDetails,
-            priceFiatRange: (
-              <ItemWUnit
-                type="mediumPrimary"
-                value={priceFiatRange}
-                unit={currentFiat}
-              />
-            ),
-          }
-        })).then((marketplaceItems) => {
-        setCollection(marketplaceItems)
-      })
+        addToCollection(resultItem)
+      }))
+        .finally(() => {
+          setCollection((curCollection) => curCollection
+            .slice(0, -1))
+        })
     }
-  }, [items, crypto, currentFiat, account, selectedProvider, providers])
+  }, [
+    cachedPlans, crypto,
+    currentFiat, account, selectedProvider, providers, addToCollection,
+  ])
 
   return (
     <MarketPageTemplate
